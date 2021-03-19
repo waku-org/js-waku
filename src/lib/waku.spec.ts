13e941513d11b4ea47de27e6d95d6f9cb5d1da7e
Franck Royer <franck@royer.one>
2021-03-19 15:03:31 +1100

    Reorg tests, add describe sections

--- src/lib/waku.spec.ts
+++ src/lib/waku.spec.ts
@@ -3,31 +3,35 @@ import { NimWaku } from '../test_utils/nim_waku';
 import Waku from './waku';
 import { CODEC } from './waku_relay';
 
-test('Nim-interop: nim-waku node connects to js node', async () => {
-  const waku = await Waku.create();
+describe('Waku', () => {
+  describe('Interop: Nim', () => {
+    test('nim connects to js', async () => {
+      const waku = await Waku.create();
 
-  const peerId = waku.libp2p.peerId.toB58String();
+      const peerId = waku.libp2p.peerId.toB58String();
 
-  const localMultiaddr = waku.libp2p.multiaddrs.find((addr) =>
-    addr.toString().match(/127\.0\.0\.1/)
-  );
-  const multiAddrWithId = localMultiaddr + '/p2p/' + peerId;
+      const localMultiaddr = waku.libp2p.multiaddrs.find((addr) =>
+        addr.toString().match(/127\.0\.0\.1/)
+      );
+      const multiAddrWithId = localMultiaddr + '/p2p/' + peerId;
 
-  const nimWaku = new NimWaku(expect.getState().currentTestName);
-  await nimWaku.start({ staticnode: multiAddrWithId });
+      const nimWaku = new NimWaku(expect.getState().currentTestName);
+      await nimWaku.start({ staticnode: multiAddrWithId });
 
-  const nimPeers = await nimWaku.peers();
+      const nimPeers = await nimWaku.peers();
 
-  expect(nimPeers).toEqual([
-    {
-      multiaddr: multiAddrWithId,
-      protocol: CODEC,
-      connected: true,
-    },
-  ]);
+      expect(nimPeers).toEqual([
+        {
+          multiaddr: multiAddrWithId,
+          protocol: CODEC,
+          connected: true,
+        },
+      ]);
 
-  const nimPeerId = await nimWaku.getPeerId();
-  const jsPeers = waku.libp2p.peerStore.peers;
+      const nimPeerId = await nimWaku.getPeerId();
+      const jsPeers = waku.libp2p.peerStore.peers;
 
-  expect(jsPeers.has(nimPeerId.toB58String())).toBeTruthy();
+      expect(jsPeers.has(nimPeerId.toB58String())).toBeTruthy();
+    });
+  });
 });
