4f8d8b099b9b86f81fd9a1b7d2cd581d0bee0770
Franck Royer <franck@status.im>
2021-07-14 11:40:15 +1000

    Setup Karma

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ['mocha', 'karma-typescript'],
+    files: [{ pattern: 'src/**/*browser.spec.ts' }],
+    preprocessors: {
+      '**/*.ts': ['karma-typescript'],
+    },
+    plugins: [
+      require('karma-mocha'),
+      require('karma-typescript'),
+      require('karma-chrome-launcher'),
+    ],
+    logLevel: config.LOG_DEBUG,
+    reporters: ['progress', 'karma-typescript'],
+    browsers: ['Chromium'],
+    singleRun: true,
+    karmaTypescriptConfig: {
+      tsconfig: './tsconfig.dev.json',
+      coverageOptions: {
+        instrumentation: false,
+      },
+    },
+  });
+};
