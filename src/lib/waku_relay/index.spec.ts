c4d0501c6aadb79a66db572ed6b242a128e69566
Franck Royer <franck@royer.one>
2021-07-26 11:04:34 +1000

    nim-waku v0.5 uses new waku relay protocol id

--- src/lib/waku_relay/index.spec.ts
+++ src/lib/waku_relay/index.spec.ts
@@ -72,7 +72,7 @@ describe('Waku Relay', () => {
     it('Register correct protocols', async function () {
       const protocols = Array.from(waku1.libp2p.upgrader.protocols.keys());
 
-      expect(protocols).to.contain('/vac/waku/relay/2.0.0-beta2');
+      expect(protocols).to.contain('/vac/waku/relay/2.0.0');
       expect(protocols.findIndex((value) => value.match(/sub/))).to.eq(-1);
     });
 
