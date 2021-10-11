b029692865448110cac6bbb1a2eb8a6890e8dec4
Franck Royer <franck@status.im>
2021-10-11 15:03:13 +1100

    Use webpack for umd

---
+++ webpack.config.bundle.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    "js-waku": './src/index.ts'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js',
+      Buffer: ['buffer', 'Buffer'],
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+    fallback: {
+      buffer: require.resolve('buffer/'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert'),
+    },
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build/umd'),
+    library: 'jswaku',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  }
+};
