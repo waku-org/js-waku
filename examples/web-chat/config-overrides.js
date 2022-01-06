cd7a8784d3595ecb0aaec787476266eebe2ac29f
Franck Royer <franck@status.im>
2022-01-06 17:39:08 +1100

    Upgrade few dependencies

---
+++ examples/web-chat/config-overrides.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = (config, env) => {
+
+  // Override webpack 5 config from react-scripts to load polyfills
+  if (!config.resolve) config.resolve = {};
+  if (!config.resolve.fallback) config.resolve.fallback = {};
+  Object.assign(config.resolve.fallback, {
+      "buffer": require.resolve("buffer"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      // "assert": require.resolve("assert")
+    }
+  )
+
+  if (!config.plugins) config.plugins = []
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.ENV': JSON.stringify(env),
+    })
+  )
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js',
+      Buffer: ['buffer', 'Buffer'],
+    }));
+
+  return config;
+}
