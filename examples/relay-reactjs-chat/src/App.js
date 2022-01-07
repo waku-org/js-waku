673869bdff1c8d5baa9665e932e9fe1341f2de38
Franck Royer <franck@status.im>
2022-01-07 17:05:39 +1100

    Use cra-webpack-rewired, display Waku status

--- examples/relay-reactjs-chat/src/App.js
+++ examples/relay-reactjs-chat/src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Waku } from 'js-waku';
+import * as React from 'react';
 
 function App() {
+  const [waku, setWaku] = React.useState(undefined);
+  const [wakuStatus, setWakuStatus] = React.useState('None');
+
+  // Start Waku
+  React.useEffect(() => {
+    // If Waku is already assigned, the job is done
+    if (!!waku) return;
+    // If Waku status not None, it means we are already starting Waku
+    if (wakuStatus !== 'None') return;
+
+    setWakuStatus('Starting');
+
+    // Create Waku
+    Waku.create({ bootstrap: true }).then((waku) => {
+      // Once done, put it in the state
+      setWaku(waku);
+      // And update the status
+      setWakuStatus('Started');
+    });
+  }, [waku, wakuStatus]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>Waku node's status: {wakuStatus}</p>
       </header>
     </div>
   );
