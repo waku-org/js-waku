eaae1e53727d2a93e1e18674aa5766215b559094
Franck Royer <franck@status.im>
2022-01-07 15:05:25 +1100

    Replace react-app-rewired with cra-webpack-rewired
    The second is more appropriate for react 5

---
+++ examples/web-chat/config/webpack.extend.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+
+module.exports = {
+  dev: (config) => {
+    // Override webpack 5 config from react-scripts to load polyfills
+    if (!config.resolve) config.resolve = {};
+    if (!config.resolve.fallback) config.resolve.fallback = {};
+    Object.assign(config.resolve.fallback, {
+        "buffer": require.resolve("buffer"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        // "assert": require.resolve("assert")
+      }
+    )
+
+    if (!config.plugins) config.plugins = []
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ENV': JSON.stringify("dev"),
+      })
+    )
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser.js',
+        Buffer: ['buffer', 'Buffer'],
+      }));
+
+    return config;
+  },
+  prod: (config) => {
+    // Override webpack 5 config from react-scripts to load polyfills
+    if (!config.resolve) config.resolve = {};
+    if (!config.resolve.fallback) config.resolve.fallback = {};
+    Object.assign(config.resolve.fallback, {
+        "buffer": require.resolve("buffer"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        // "assert": require.resolve("assert")
+      }
+    )
+
+    if (!config.plugins) config.plugins = []
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ENV': JSON.stringify("prod"),
+      })
+    )
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser.js',
+        Buffer: ['buffer', 'Buffer'],
+      }));
+
+    return config;
+  }
+};
