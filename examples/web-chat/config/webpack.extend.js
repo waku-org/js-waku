add78bce718078c60e9b49e594b718170b126925
Franck Royer <franck@status.im>
2022-01-07 15:18:37 +1100

    Format js config file

--- examples/web-chat/config/webpack.extend.js
+++ examples/web-chat/config/webpack.extend.js
@@ -6,27 +6,27 @@ module.exports = {
     if (!config.resolve) config.resolve = {};
     if (!config.resolve.fallback) config.resolve.fallback = {};
     Object.assign(config.resolve.fallback, {
-        "buffer": require.resolve("buffer"),
-        "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        // "assert": require.resolve("assert")
-      }
-    )
+      buffer: require.resolve('buffer'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      // "assert": require.resolve("assert")
+    });
 
-    if (!config.plugins) config.plugins = []
+    if (!config.plugins) config.plugins = [];
     config.plugins.push(
       new webpack.DefinePlugin({
-        'process.env.ENV': JSON.stringify("dev"),
+        'process.env.ENV': JSON.stringify('dev'),
       })
-    )
+    );
     config.plugins.push(
       new webpack.ProvidePlugin({
         process: 'process/browser.js',
         Buffer: ['buffer', 'Buffer'],
-      }));
+      })
+    );
 
-    if (!config.ignoreWarnings) config.ignoreWarnings = []
-    config.ignoreWarnings.push(/Failed to parse source map/)
+    if (!config.ignoreWarnings) config.ignoreWarnings = [];
+    config.ignoreWarnings.push(/Failed to parse source map/);
 
     return config;
   },
@@ -35,28 +35,28 @@ module.exports = {
     if (!config.resolve) config.resolve = {};
     if (!config.resolve.fallback) config.resolve.fallback = {};
     Object.assign(config.resolve.fallback, {
-        "buffer": require.resolve("buffer"),
-        "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        // "assert": require.resolve("assert")
-      }
-    )
+      buffer: require.resolve('buffer'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      // "assert": require.resolve("assert")
+    });
 
-    if (!config.plugins) config.plugins = []
+    if (!config.plugins) config.plugins = [];
     config.plugins.push(
       new webpack.DefinePlugin({
-        'process.env.ENV': JSON.stringify("prod"),
+        'process.env.ENV': JSON.stringify('prod'),
       })
-    )
+    );
     config.plugins.push(
       new webpack.ProvidePlugin({
         process: 'process/browser.js',
         Buffer: ['buffer', 'Buffer'],
-      }));
+      })
+    );
 
-    if (!config.ignoreWarnings) config.ignoreWarnings = []
-    config.ignoreWarnings.push(/Failed to parse source map/)
+    if (!config.ignoreWarnings) config.ignoreWarnings = [];
+    config.ignoreWarnings.push(/Failed to parse source map/);
 
     return config;
-  }
+  },
 };
