80cfdfa94f567e97fd3318beb531a3ba0c694d46
jemboh <jemboh@users.noreply.github.com>
2022-02-07 16:51:42 +0000

    added messages component to display messages

--- examples/relay-angular-chat/src/app/app.module.ts
+++ examples/relay-angular-chat/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule
