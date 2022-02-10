a11ef37467e297eec8ae3ae7746ba984da6b0021
jemboh <jemboh@users.noreply.github.com>
2022-02-10 20:31:06 +0000

    [wip] removed message service for now

--- examples/relay-angular-chat/src/app/app.module.ts
+++ examples/relay-angular-chat/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MessagesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
