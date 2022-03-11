90af0512bcbc2695cbb8328f05ece4c07a3c779f
Franck Royer <franck@status.im>
2022-03-11 23:27:37 +1100

    Add light push for test

--- src/lib/nwaku.spec.ts
+++ src/lib/nwaku.spec.ts
@@ -351,6 +351,73 @@ describe("Test nwaku fleets", () => {
       }
     });
 
+    it("Light Push", async function () {
+      // This test depends on fleets.status.im being online.
+      // This dependence must be removed once DNS discovery is implemented
+      this.timeout(60000);
+
+      const id = randomBytes(4).toString();
+
+      const nodes = Object.values(
+        fleets.fleets["wakuv2.test"]["waku-websocket"]
+      );
+
+      expect(nodes.length).to.eq(3);
+
+      const promises = nodes.map(async (node, i) => {
+        wakus[i] = await Waku.create({
+          bootstrap: { peers: [node] },
+        });
+
+        await wakus[i].waitForRemotePeer([Protocols.LightPush]);
+        console.log(node + ": ready");
+      });
+
+      await Promise.all(promises);
+      // All connected and relay ready
+
+      const contentTopic = "/js-waku-testing/1/relay-test/utf8";
+
+      const messages: Array<{ msg: string; timestamp: Date; rcvd: Date }> = [];
+
+      wakus.forEach((waku) => {
+        waku.relay.addObserver(
+          (message) => {
+            messages.push({
+              msg: message.payloadAsUtf8,
+              timestamp: message.timestamp!,
+              rcvd: new Date(),
+            });
+          },
+          [contentTopic]
+        );
+      });
+
+      const relayPromises = wakus.map(async (waku, i) => {
+        const msg = await WakuMessage.fromUtf8String(
+          `sent from ${i} - ${id}`,
+          contentTopic
+        );
+        return waku.lightPush.push(msg);
+      });
+
+      await Promise.all(relayPromises);
+      await delay(30000);
+
+      console.log(messages);
+
+      messages.forEach((msg) => {
+        const diff = msg.rcvd.getTime() - msg.timestamp.getTime();
+        console.log(msg.timestamp, msg.rcvd, diff + "ms");
+      });
+
+      expect(messages.length).to.gte(nodes.length);
+
+      for (let i = 0; i < wakus.length; i++) {
+        expect(messages.map((m) => m.msg)).to.contain(`sent from ${i} - ${id}`);
+      }
+    });
+
     it("Store", async function () {
       // This test depends on fleets.status.im being online.
       // This dependence must be removed once DNS discovery is implemented
