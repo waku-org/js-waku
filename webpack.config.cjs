c4758a8737517c0302f90f3c4bf3a5e2f9c86e2e
Franck Royer <franck@status.im>
2022-05-27 23:01:52 +1000

    Setup rollup, move files to dist/

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.(js|tsx?)$/,
         use: "ts-loader",
         exclude: /(node_modules)|(node\.spec\.ts)/,
       },
