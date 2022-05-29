598a6a120eeb902f996a5881d305bf05ee3ef320
Felicio Mununga <felicio@users.noreply.github.com>
2022-05-29 17:41:55 +0200

    add note

--- src/lib/waku.ts
+++ src/lib/waku.ts
@@ -138,6 +138,7 @@ export class Waku {
     /**
      * NOTE: Event is not being emitted on closing nor loosing a connection.
      * @see https://github.com/libp2p/js-libp2p/issues/939
+     * @see https://github.com/status-im/js-waku/issues/252
      *
      * >This event will be triggered anytime we are disconnected from another peer,
      * >regardless of the circumstances of that disconnection.
