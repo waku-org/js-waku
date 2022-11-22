fa1418c565cd4a01c6fecd28fffdbab5b349373a
danisharora099 <danisharora099@gmail.com>
2022-11-22 22:37:50 +0530

    refactor peer-exchange test

--- packages/tests/tests/peer_exchange.node.spec.ts
+++ packages/tests/tests/peer_exchange.node.spec.ts
@@ -4,35 +4,69 @@ import type { WakuLight } from "@waku/interfaces";
 import { Protocols } from "@waku/interfaces";
 
 import { makeLogFileName, Nwaku } from "../src";
+import { delay } from "../src/delay";
 
 describe("Peer Exchange: Node", () => {
   let waku: WakuLight;
-  let nwaku: Nwaku;
+  let nwaku1: Nwaku;
+  let nwaku2: Nwaku;
+  let nwaku3: Nwaku;
 
   afterEach(async function () {
-    !!nwaku && nwaku.stop();
+    !!nwaku1 && nwaku1.stop();
+    !!nwaku2 && nwaku2.stop();
+    !!nwaku3 && nwaku3.stop();
     !!waku && waku.stop().catch((e) => console.log("Waku failed to stop", e));
   });
 
   it("Queries successfully", async function () {
-    this.timeout(55_000);
+    this.timeout(15_000);
 
-    nwaku = new Nwaku(makeLogFileName(this));
-    await nwaku.start({ peerExchange: true, discv5Discovery: true });
+    console.log("starting");
+    nwaku1 = new Nwaku(makeLogFileName(this));
+    nwaku2 = new Nwaku(makeLogFileName(this));
+    nwaku3 = new Nwaku(makeLogFileName(this));
+
+    await nwaku2.start({ discv5Discovery: true, peerExchange: true });
+    console.log("nwaku2 started");
+
+    const enr = (await nwaku2.info()).enrUri;
+    console.log({ enr });
+
+    await nwaku1.start({
+      discv5Discovery: true,
+      manualArgs: [
+        `--discv5-bootstrap-node:${(await nwaku2.info()).enrUri}`,
+        `--ports-shift=5`,
+      ],
+    });
+    console.log("nwaku1 started");
+
+    await nwaku3.start({
+      discv5Discovery: true,
+      manualArgs: [
+        `--discv5-bootstrap-node:${(await nwaku2.info()).enrUri}`,
+        `--ports-shift=10`,
+      ],
+    });
 
     waku = await createLightNode();
+    console.log("created light node");
     await waku.start();
+    console.log("started light node");
 
-    const mulltiaddr = await nwaku.getMultiaddrWithId();
+    await delay(1000);
+
+    const mulltiaddr = await nwaku2.getMultiaddrWithId();
+
     await waku.dial(mulltiaddr, [Protocols.PeerExchange]);
+    console.log("dialed");
 
-    try {
-      await waitForRemotePeer(waku, [Protocols.PeerExchange]);
-      console.log("waited");
-    } catch (error) {
-      console.error(error);
-    }
-    console.log("remote peer found");
+    await waitForRemotePeer(waku, [Protocols.PeerExchange]);
+    console.log("waited");
+
+    await delay(3000);
+
     try {
       const queryResponse = await waku.peerExchange.query({
         numPeers: BigInt(1),
