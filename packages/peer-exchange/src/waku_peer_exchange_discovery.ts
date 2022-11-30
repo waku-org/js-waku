9539d531929c3b4552fab9105312dd9d52aa0e8b
danisharora099 <danisharora099@gmail.com>
2022-11-30 14:04:19 +0530

    refactor

--- packages/peer-exchange/src/waku_peer_exchange_discovery.ts
+++ packages/peer-exchange/src/waku_peer_exchange_discovery.ts
@@ -5,23 +5,14 @@ import {
 } from "@libp2p/interface-peer-discovery";
 import { PeerInfo } from "@libp2p/interface-peer-info";
 import { EventEmitter } from "@libp2p/interfaces/events";
-import { PeerExchangeComponents, PeerExchangeResponse } from "@waku/interfaces";
+import { PeerExchangeComponents } from "@waku/interfaces";
 import debug from "debug";
 
-import { WakuPeerExchange } from "./waku_peer_exchange";
+import { PeerExchangeCodec } from "./waku_peer_exchange";
 
 const log = debug("waku:peer-exchange-discovery");
 
 interface Options {
-  /**
-   * The number of peers to request for.
-   */
-  numPeers: number;
-  /**
-   * How long to wait before discovering bootstrap nodes
-   */
-  timeout?: number;
-
   /**
    * Tag a bootstrap peer with this name before "discovering" it (default: 'bootstrap')
    */
@@ -41,114 +32,67 @@ interface Options {
 const DEFAULT_BOOTSTRAP_TAG_NAME = "peer-exchange";
 const DEFAULT_BOOTSTRAP_TAG_VALUE = 50;
 const DEFAULT_BOOTSTRAP_TAG_TTL = 120000;
-const DEFAULT_BOOTSTRAP_DISCOVERY_TIMEOUT = 1000;
 
 export class PeerExchangeDiscovery
   extends EventEmitter<PeerDiscoveryEvents>
   implements PeerDiscovery
 {
-  private timer?: ReturnType<typeof setTimeout>;
   private readonly components: PeerExchangeComponents;
   private readonly options: Options;
-  private readonly timeout: number;
-  private readonly peerExchange: WakuPeerExchange;
+  private isStarted: boolean;
 
-  constructor(
-    components: PeerExchangeComponents,
-    options: Options = { numPeers: 3 }
-  ) {
+  constructor(components: PeerExchangeComponents, options: Options) {
     super();
     this.components = components;
     this.options = options;
-    this.timeout = DEFAULT_BOOTSTRAP_DISCOVERY_TIMEOUT;
-    this.peerExchange = new WakuPeerExchange(this.components);
+    this.isStarted = false;
   }
 
   /**
    * Start emitting events
    */
   start(): void {
-    if (this.isStarted()) {
+    if (this.isStarted) {
       return;
     }
 
-    log(
-      "Starting bootstrap node discovery, discovering peers after %s ms",
-      this.timeout
+    log("Starting peer exchange node discovery, discovering peers");
+
+    this.components.peerStore.addEventListener(
+      "change:protocols",
+      async (event) => {
+        const { protocols } = event.detail;
+        if (!protocols.includes(PeerExchangeCodec)) return;
+
+        const { peerId } = event.detail;
+        const peer = await this.components.peerStore.get(peerId);
+        const peerInfo = {
+          id: peerId,
+          multiaddrs: peer.addresses.map((address) => address.multiaddr),
+          protocols: [],
+        };
+        await this.components.peerStore.tagPeer(
+          peerId,
+          DEFAULT_BOOTSTRAP_TAG_NAME,
+          {
+            value: this.options.tagValue ?? DEFAULT_BOOTSTRAP_TAG_VALUE,
+            ttl: this.options.tagTTL ?? DEFAULT_BOOTSTRAP_TAG_TTL,
+          }
+        );
+        this.dispatchEvent(
+          new CustomEvent<PeerInfo>("peer", { detail: peerInfo })
+        );
+      }
     );
-    this.timer = setTimeout(() => {
-      this.requestPeers();
-    }, this.timeout);
   }
 
   /**
-   * Stop emitting events
+   * Remove event listener
    */
   stop(): void {
-    if (this.timer != null) {
-      clearTimeout(this.timer);
-    }
-
-    this.timer = undefined;
-  }
-
-  requestPeers(): void {
-    this.peerExchange.query(
-      { numPeers: BigInt(1) },
-      this._callbackToEmit.bind(this)
-    );
-  }
-
-  /**
-   * Emit address as a PeerInfo
-   */
-  async _callbackToEmit(response: PeerExchangeResponse): Promise<void> {
-    if (this.timer == null) {
-      return;
-    }
-
-    const { peerInfos } = response;
-
-    peerInfos.forEach(async (peerInfo) => {
-      const { ENR } = peerInfo;
-      if (!ENR) return;
-
-      const peerData = {
-        id: ENR.peerId,
-        multiaddrs: ENR.multiaddrs,
-        protocols: [],
-      };
-
-      if (
-        !peerData ||
-        !peerData.id ||
-        !peerData.multiaddrs ||
-        peerData.multiaddrs.length === 0
-      )
-        return;
-
-      await this.components.peerStore.tagPeer(
-        peerData.id,
-        DEFAULT_BOOTSTRAP_TAG_NAME,
-        {
-          value: this.options.tagValue ?? DEFAULT_BOOTSTRAP_TAG_VALUE,
-          ttl: this.options.tagTTL ?? DEFAULT_BOOTSTRAP_TAG_TTL,
-        }
-      );
-
-      // check we are still running
-      if (this.timer == null) {
-        return;
-      }
-
-      this.dispatchEvent(
-        new CustomEvent<PeerInfo>("peer", { detail: peerData as PeerInfo })
-      );
-    });
-  }
-
-  isStarted(): boolean {
-    return Boolean(this.timer);
+    if (!this.isStarted) return;
+    log("Stopping peer exchange node discovery");
+    this.components.peerStore.removeEventListener("change:protocols");
   }
 
   get [symbol](): true {
