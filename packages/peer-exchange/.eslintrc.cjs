6540fe7979cd7e83e02028c65f739868bcba1d8e
Danish Arora <35004822+danisharora099@users.noreply.github.com>
2022-12-02 10:03:37 +0530

    Update packages/peer-exchange/.eslintrc.cjs
    Co-authored-by: fryorcraken.eth <110212804+fryorcraken@users.noreply.github.com>

--- packages/peer-exchange/.eslintrc.cjs
+++ packages/peer-exchange/.eslintrc.cjs
@@ -1,27 +1,6 @@
 module.exports = {
-  // ...
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  env: {
-    node: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.dev.json",
   },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
-
-      // As mentioned in the comments, you should extend TypeScript plugins here,
-      // instead of extending them outside the `overrides`.
-      // If you don't want to extend any rules, you don't need an `extends` attribute.
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: "./tsconfig.dev.json",
-      },
-    },
-  ],
-  // ...
 };
