8be9ed7de15dcbc6ac44da116c14d17ae8f4fba1
danisharora099 <danisharora099@gmail.com>
2022-12-05 09:31:15 +0530

    address comments

--- packages/peer-exchange/src/waku_peer_exchange_discovery.ts
+++ packages/peer-exchange/src/waku_peer_exchange_discovery.ts
@@ -4,6 +4,7 @@ import {
   symbol,
 } from "@libp2p/interface-peer-discovery";
 import { PeerInfo } from "@libp2p/interface-peer-info";
+import { PeerProtocolsChangeData } from "@libp2p/interface-peer-store";
 import { EventEmitter } from "@libp2p/interfaces/events";
 import { PeerExchangeComponents } from "@waku/interfaces";
 import debug from "debug";
@@ -41,6 +42,30 @@ export class PeerExchangeDiscovery
   private readonly options: Options;
   private isStarted: boolean;
 
+  private readonly eventHandler = async (
+    event: CustomEvent<PeerProtocolsChangeData>
+  ): Promise<void> => {
+    const { protocols } = event.detail;
+    if (!protocols.includes(PeerExchangeCodec)) return;
+
+    const { peerId } = event.detail;
+    const peer = await this.components.peerStore.get(peerId);
+    const peerInfo = {
+      id: peerId,
+      multiaddrs: peer.addresses.map((address) => address.multiaddr),
+      protocols: [],
+    };
+    await this.components.peerStore.tagPeer(
+      peerId,
+      DEFAULT_BOOTSTRAP_TAG_NAME,
+      {
+        value: this.options.tagValue ?? DEFAULT_BOOTSTRAP_TAG_VALUE,
+        ttl: this.options.tagTTL ?? DEFAULT_BOOTSTRAP_TAG_TTL,
+      }
+    );
+    this.dispatchEvent(new CustomEvent<PeerInfo>("peer", { detail: peerInfo }));
+  };
+
   constructor(components: PeerExchangeComponents, options: Options = {}) {
     super();
     this.components = components;
@@ -58,31 +83,10 @@ export class PeerExchangeDiscovery
 
     log("Starting peer exchange node discovery, discovering peers");
 
-    this.components.peerStore.addEventListener("change:protocols", (event) => {
-      (async () => {
-        const { protocols } = event.detail;
-        if (!protocols.includes(PeerExchangeCodec)) return;
-
-        const { peerId } = event.detail;
-        const peer = await this.components.peerStore.get(peerId);
-        const peerInfo = {
-          id: peerId,
-          multiaddrs: peer.addresses.map((address) => address.multiaddr),
-          protocols: [],
-        };
-        await this.components.peerStore.tagPeer(
-          peerId,
-          DEFAULT_BOOTSTRAP_TAG_NAME,
-          {
-            value: this.options.tagValue ?? DEFAULT_BOOTSTRAP_TAG_VALUE,
-            ttl: this.options.tagTTL ?? DEFAULT_BOOTSTRAP_TAG_TTL,
-          }
-        );
-        this.dispatchEvent(
-          new CustomEvent<PeerInfo>("peer", { detail: peerInfo })
-        );
-      })().catch((err) => log("Failed to handle peer exchange request", err));
-    });
+    this.components.peerStore.addEventListener(
+      "change:protocols",
+      this.eventHandler
+    );
   }
 
   /**
@@ -91,7 +95,11 @@ export class PeerExchangeDiscovery
   stop(): void {
     if (!this.isStarted) return;
     log("Stopping peer exchange node discovery");
-    this.components.peerStore.removeEventListener("change:protocols");
+    this.isStarted = false;
+    this.components.peerStore.removeEventListener(
+      "change:protocols",
+      this.eventHandler
+    );
   }
 
   get [symbol](): true {
