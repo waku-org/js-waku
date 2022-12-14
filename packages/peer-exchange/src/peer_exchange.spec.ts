f7df3513c02771cc97cb5112989ef2408286a233
danisharora099 <danisharora099@gmail.com>
2022-12-14 11:14:13 +0530

    init: test for libp2p bootstrap/discovery for peer-exchange

---
+++ packages/peer-exchange/src/peer_exchange.spec.ts
@@ -0,0 +1,44 @@
+import { bootstrap } from "@libp2p/bootstrap";
+import { waitForRemotePeer } from "@waku/core";
+import {
+  Fleet,
+  getPredefinedBootstrapNodes,
+} from "@waku/core/lib/predefined_bootstrap_nodes";
+import { createLightNode } from "@waku/create";
+import { Protocols, Waku } from "@waku/interfaces";
+
+import { delay } from "../../tests/src";
+
+import { wakuPeerExchangeDiscovery } from "./waku_peer_exchange_discovery";
+
+describe.only("Peer Exchange Discovery", () => {
+  let waku: Waku;
+
+  afterEach(async function () {
+    !!waku && waku.stop().catch((e) => console.log("Waku failed to stop", e));
+  });
+  it("connects to nwaku", async function () {
+    try {
+      this.timeout(120_000);
+
+      waku = await createLightNode({
+        libp2p: {
+          peerDiscovery: [
+            bootstrap({ list: getPredefinedBootstrapNodes(Fleet.Test) }),
+            wakuPeerExchangeDiscovery(),
+          ],
+        },
+      });
+
+      await waku.start();
+      await delay(1000);
+
+      while (waku.libp2p.connectionManager.getConnections().length >= 2) {
+        await waitForRemotePeer(waku, [Protocols.PeerExchange]);
+        await delay(3000);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
