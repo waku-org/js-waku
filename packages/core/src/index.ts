3134d0cf2dcb3bc420967534cc5c7516f355e2d5
danisharora099 <danisharora099@gmail.com>
2023-02-20 14:35:27 +0530

    remove stale export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -32,7 +32,6 @@ export { waitForRemotePeer } from "./lib/wait_for_remote_peer.js";
 
 export {
   ConnectionManager,
-  Libp2pComponents,
   ConnectionManagerOptions,
 } from "./lib/connection_manager.js";
 
