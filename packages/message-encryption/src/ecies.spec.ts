497588bc360b4a77fb8af5081234b83c5eb269d4
fryorcraken.eth <git@fryorcraken.xyz>
2023-03-13 14:15:57 +1100

    chore: make topics arbitrary data

--- packages/message-encryption/src/ecies.spec.ts
+++ packages/message-encryption/src/ecies.spec.ts
@@ -4,35 +4,31 @@ import fc from "fast-check";
 import { getPublicKey } from "./crypto/index.js";
 import { createDecoder, createEncoder } from "./ecies.js";
 
-const TestContentTopic = "/test/1/waku-message/utf8";
-const TestPubSubTopic = "/test/pubsub/topic";
-
 describe("Ecies Encryption", function () {
   it("Round trip binary encryption [ecies, no signature]", async function () {
     await fc.assert(
       fc.asyncProperty(
+        fc.string(),
+        fc.string(),
         fc.uint8Array({ minLength: 1 }),
         fc.uint8Array({ min: 1, minLength: 32, maxLength: 32 }),
-        async (payload, privateKey) => {
+        async (pubSubTopic, contentTopic, payload, privateKey) => {
           const publicKey = getPublicKey(privateKey);
 
           const encoder = createEncoder({
-            contentTopic: TestContentTopic,
+            contentTopic,
             publicKey,
           });
           const bytes = await encoder.toWire({ payload });
 
-          const decoder = createDecoder(TestContentTopic, privateKey);
+          const decoder = createDecoder(contentTopic, privateKey);
           const protoResult = await decoder.fromWireToProtoObj(bytes!);
           if (!protoResult) throw "Failed to proto decode";
-          const result = await decoder.fromProtoObj(
-            TestPubSubTopic,
-            protoResult
-          );
+          const result = await decoder.fromProtoObj(pubSubTopic, protoResult);
           if (!result) throw "Failed to decode";
 
-          expect(result.contentTopic).to.equal(TestContentTopic);
-          expect(result.pubSubTopic).to.equal(TestPubSubTopic);
+          expect(result.contentTopic).to.equal(contentTopic);
+          expect(result.pubSubTopic).to.equal(pubSubTopic);
           expect(result.version).to.equal(1);
           expect(result?.payload).to.deep.equal(payload);
           expect(result.signature).to.be.undefined;
@@ -47,31 +43,36 @@ describe("Ecies Encryption", function () {
 
     await fc.assert(
       fc.asyncProperty(
+        fc.string(),
+        fc.string(),
         fc.uint8Array({ minLength: 1 }),
         fc.uint8Array({ min: 1, minLength: 32, maxLength: 32 }),
         fc.uint8Array({ min: 1, minLength: 32, maxLength: 32 }),
-        async (payload, alicePrivateKey, bobPrivateKey) => {
+        async (
+          pubSubTopic,
+          contentTopic,
+          payload,
+          alicePrivateKey,
+          bobPrivateKey
+        ) => {
           const alicePublicKey = getPublicKey(alicePrivateKey);
           const bobPublicKey = getPublicKey(bobPrivateKey);
 
           const encoder = createEncoder({
-            contentTopic: TestContentTopic,
+            contentTopic,
             publicKey: bobPublicKey,
             sigPrivKey: alicePrivateKey,
           });
           const bytes = await encoder.toWire({ payload });
 
-          const decoder = createDecoder(TestContentTopic, bobPrivateKey);
+          const decoder = createDecoder(contentTopic, bobPrivateKey);
           const protoResult = await decoder.fromWireToProtoObj(bytes!);
           if (!protoResult) throw "Failed to proto decode";
-          const result = await decoder.fromProtoObj(
-            TestPubSubTopic,
-            protoResult
-          );
+          const result = await decoder.fromProtoObj(pubSubTopic, protoResult);
           if (!result) throw "Failed to decode";
 
-          expect(result.contentTopic).to.equal(TestContentTopic);
-          expect(result.pubSubTopic).to.equal(TestPubSubTopic);
+          expect(result.contentTopic).to.equal(contentTopic);
+          expect(result.pubSubTopic).to.equal(pubSubTopic);
           expect(result.version).to.equal(1);
           expect(result?.payload).to.deep.equal(payload);
           expect(result.signature).to.not.be.undefined;
