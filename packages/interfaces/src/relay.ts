bda01f43ebf3bcc1b054b42b42ca4a8dea071465
danisharora099 <danisharora099@gmail.com>
2023-05-03 12:15:22 +0530

    update relay types

--- packages/interfaces/src/relay.ts
+++ packages/interfaces/src/relay.ts
@@ -1,7 +1,9 @@
 import type { GossipSub } from "@chainsafe/libp2p-gossipsub";
 import type { PeerIdStr, TopicStr } from "@chainsafe/libp2p-gossipsub/types";
 
-import { IReceiverV1 } from "./receiver.js";
+import { IDecodedMessage, IDecoder } from "./message.js";
+import { Callback, ProtocolOptions } from "./protocols.js";
+import { IReceiver } from "./receiver.js";
 import type { ISender } from "./sender.js";
 
 interface IRelayAPI {
@@ -10,4 +12,12 @@ interface IRelayAPI {
   getMeshPeers: (topic?: TopicStr) => PeerIdStr[];
 }
 
-export type IRelay = IRelayAPI & ISender & IReceiverV1;
+export type IRelay = IRelayAPI &
+  ISender &
+  Omit<IReceiver<"v1">, "unsubscribeAll" | "subscribe"> & {
+    subscribe: <T extends IDecodedMessage>(
+      decoders: IDecoder<T> | IDecoder<T>[],
+      callback: Callback<T>,
+      opts?: ProtocolOptions
+    ) => () => void;
+  };
