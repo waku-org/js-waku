ef87af6e7ad707034c0b8bda7af98167d4408d05
Sasha <118575614+weboko@users.noreply.github.com>
2023-05-17 10:38:20 +0200

    chore: add iterator timeout and unblock main thread (#1357)
    * add iterator timeout
    * add idle state to generator
    * add js-doc and iterator option

--- packages/utils/src/common/to_async_iterator.ts
+++ packages/utils/src/common/to_async_iterator.ts
@@ -7,11 +7,29 @@ import type {
   Unsubscribe,
 } from "@waku/interfaces";
 
+type IteratorOptions = {
+  timeoutMs?: number;
+  iteratorDelay?: number;
+};
+
+const FRAME_RATE = 60;
+
+/**
+ * Function that transforms IReceiver subscription to iterable stream of data.
+ * @param receiver - object that allows to be subscribed to;
+ * @param decoder - parameter to be passed to receiver for subscription;
+ * @param options - options for receiver for subscription;
+ * @param iteratorOptions - optional configuration for iterator;
+ * @returns iterator and stop function to terminate it.
+ */
 export async function toAsyncIterator<T extends IDecodedMessage>(
   receiver: IReceiver,
   decoder: IDecoder<T> | IDecoder<T>[],
-  options?: ProtocolOptions
+  options?: ProtocolOptions,
+  iteratorOptions?: IteratorOptions
 ): Promise<IAsyncIterator<T>> {
+  const iteratorDelay = iteratorOptions?.iteratorDelay ?? FRAME_RATE;
+
   const messages: T[] = [];
 
   let unsubscribe: undefined | Unsubscribe;
@@ -23,8 +41,18 @@ export async function toAsyncIterator<T extends IDecodedMessage>(
     options
   );
 
+  const isWithTimeout = Number.isInteger(iteratorOptions?.timeoutMs);
+  const timeoutMs = iteratorOptions?.timeoutMs ?? 0;
+  const startTime = Date.now();
+
   async function* iterator(): AsyncIterator<T> {
     while (true) {
+      if (isWithTimeout && Date.now() - startTime >= timeoutMs) {
+        return;
+      }
+
+      await wait(iteratorDelay);
+
       const message = messages.shift() as T;
 
       if (!unsubscribe && messages.length === 0) {
@@ -48,4 +76,10 @@ export async function toAsyncIterator<T extends IDecodedMessage>(
       }
     },
   };
+}
+
+function wait(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
