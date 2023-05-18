b4f38f7fb43309fe0d11329e7bc204abb80d172b
danisharora099 <danisharora099@gmail.com>
2023-05-19 01:30:41 +0530

    rename file to node.ts

--- packages/tests/src/index.ts
+++ packages/tests/src/index.ts
@@ -9,4 +9,4 @@ export * from "./async_fs.js";
 export * from "./constants.js";
 export * from "./delay.js";
 export * from "./log_file.js";
-export * from "./node/nwaku.js";
+export * from "./node/node.js";
