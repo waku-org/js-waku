ecc63ed60bf5c7eef25fcb50feeb18a7ecf2e073
danisharora099 <danisharora099@gmail.com>
2023-05-30 00:19:16 +0530

    (v0.45): adhere to new interfaces/types for libp2p

--- packages/interfaces/src/libp2p.ts
+++ packages/interfaces/src/libp2p.ts
@@ -1,24 +1,38 @@
+import { GossipSub } from "@chainsafe/libp2p-gossipsub";
 import type { AddressManager } from "@libp2p/interface-address-manager";
 import type { ConnectionProtector } from "@libp2p/interface-connection";
 import type { ConnectionGater } from "@libp2p/interface-connection-gater";
-import type {
-  ConnectionManager,
-  Dialer,
-} from "@libp2p/interface-connection-manager";
+import type { ConnectionManager } from "@libp2p/interface-connection-manager";
 import type { ContentRouting } from "@libp2p/interface-content-routing";
-import type { DualDHT } from "@libp2p/interface-dht";
+import type { Libp2p as BaseLibp2p } from "@libp2p/interface-libp2p";
+import type { Libp2pEvents } from "@libp2p/interface-libp2p";
 import type { Metrics } from "@libp2p/interface-metrics";
 import type { PeerId } from "@libp2p/interface-peer-id";
 import type { PeerRouting } from "@libp2p/interface-peer-routing";
 import type { PeerStore } from "@libp2p/interface-peer-store";
-import type { PubSub } from "@libp2p/interface-pubsub";
 import type { Registrar } from "@libp2p/interface-registrar";
 import type { TransportManager, Upgrader } from "@libp2p/interface-transport";
+import type { EventEmitter } from "@libp2p/interfaces/events";
+import type { Startable } from "@libp2p/interfaces/startable";
 import type { Datastore } from "interface-datastore";
+import type { Libp2pOptions as BaseLibp2pOptions } from "libp2p";
+import { identifyService } from "libp2p/identify";
+import type { PingService } from "libp2p/ping";
+
+type Services = {
+  ping: PingService;
+  identify: typeof identifyService;
+  pubsub?: GossipSub;
+};
+
+export type Libp2pOptions = BaseLibp2pOptions<Services>;
+export type Libp2p = BaseLibp2p<Services>;
 
 // TODO: Get libp2p to export this.
-export interface Libp2pComponents {
+
+export interface Libp2pComponents extends Record<string, any>, Startable {
   peerId: PeerId;
+  events: EventEmitter<Libp2pEvents>;
   addressManager: AddressManager;
   peerStore: PeerStore;
   upgrader: Upgrader;
@@ -30,8 +44,5 @@ export interface Libp2pComponents {
   peerRouting: PeerRouting;
   datastore: Datastore;
   connectionProtector?: ConnectionProtector;
-  dialer: Dialer;
   metrics?: Metrics;
-  dht?: DualDHT;
-  pubsub?: PubSub;
 }
