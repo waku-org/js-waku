c02f22486c7461e8f89611319606c07c521d9e4f
Sasha <oleksandr@status.im>
2023-06-14 01:06:45 +0200

    complete relay

--- packages/relay/src/index.ts
+++ packages/relay/src/index.ts
@@ -6,7 +6,6 @@ import {
 } from "@chainsafe/libp2p-gossipsub";
 import type { PeerIdStr, TopicStr } from "@chainsafe/libp2p-gossipsub/types";
 import { SignaturePolicy } from "@chainsafe/libp2p-gossipsub/types";
-import type { Libp2p } from "@libp2p/interface-libp2p";
 import type { PubSub } from "@libp2p/interface-pubsub";
 import { sha256 } from "@noble/hashes/sha256";
 import { DefaultPubSubTopic } from "@waku/core";
@@ -19,6 +18,7 @@ import {
   IEncoder,
   IMessage,
   IRelay,
+  Libp2p,
   ProtocolCreateOptions,
   ProtocolOptions,
   SendError,
@@ -59,13 +59,13 @@ class Relay implements IRelay {
   private observers: Map<ContentTopic, Set<unknown>>;
 
   constructor(libp2p: Libp2p, options?: Partial<RelayCreateOptions>) {
-    if (!this.isRelayPubSub(libp2p.pubsub)) {
+    if (!this.isRelayPubSub(libp2p.services.pubsub)) {
       throw Error(
         `Failed to initialize Relay. libp2p.pubsub does not support ${Relay.multicodec}`
       );
     }
 
-    this.gossipSub = libp2p.pubsub as GossipSub;
+    this.gossipSub = libp2p.services.pubsub as GossipSub;
     this.pubSubTopic = options?.pubSubTopic ?? DefaultPubSubTopic;
 
     if (this.gossipSub.isStarted()) {
@@ -232,7 +232,7 @@ class Relay implements IRelay {
     this.gossipSub.subscribe(pubSubTopic);
   }
 
-  private isRelayPubSub(pubsub: PubSub): boolean {
+  private isRelayPubSub(pubsub: PubSub | undefined): boolean {
     return pubsub?.multicodecs?.includes(Relay.multicodec) || false;
   }
 }
