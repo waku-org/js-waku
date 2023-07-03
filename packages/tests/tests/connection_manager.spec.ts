2482bb2092e920be8c68e72c00028159713f4ec5
danisharora099 <danisharora099@gmail.com>
2023-07-03 17:23:18 +0100

    add tests for peer-exchange

--- packages/tests/tests/connection_manager.spec.ts
+++ packages/tests/tests/connection_manager.spec.ts
@@ -14,7 +14,7 @@ const KEEP_ALIVE_OPTIONS: KeepAliveOptions = {
 const TEST_TIMEOUT = 10_000;
 const DELAY_MS = 1_000;
 
-describe("ConnectionManager", function () {
+describe.only("ConnectionManager", function () {
   let connectionManager: ConnectionManager | undefined;
   let waku: LightNode;
   let peerId: string;
@@ -84,63 +84,113 @@ describe("ConnectionManager", function () {
       getConnectionsStub.restore();
     });
 
-    it("should be called for bootstrap peers", async function () {
-      this.timeout(TEST_TIMEOUT);
+    describe("For bootstrap peers", function () {
+      it("should be called for bootstrap peers", async function () {
+        this.timeout(TEST_TIMEOUT);
 
-      // simulate that the peer is not connected
-      getConnectionsStub.returns([]);
+        // simulate that the peer is not connected
+        getConnectionsStub.returns([]);
 
-      // simulate that the peer is a bootstrap peer
-      getTagNamesForPeerStub.resolves([Tags.BOOTSTRAP]);
+        // simulate that the peer is a bootstrap peer
+        getTagNamesForPeerStub.resolves([Tags.BOOTSTRAP]);
 
-      // emit a peer:discovery event
-      waku.libp2p.dispatchEvent(
-        new CustomEvent("peer:discovery", { detail: "bootstrap-peer" })
-      );
+        // emit a peer:discovery event
+        waku.libp2p.dispatchEvent(
+          new CustomEvent("peer:discovery", { detail: "bootstrap-peer" })
+        );
 
-      // wait for the async function calls within attemptDial to finish
-      await delay(DELAY_MS);
+        // wait for the async function calls within attemptDial to finish
+        await delay(DELAY_MS);
 
-      // check that dialPeer was called once
-      expect(dialPeerStub.callCount).to.equal(
-        1,
-        "dialPeer should be called for bootstrap peers"
-      );
+        // check that dialPeer was called once
+        expect(dialPeerStub.callCount).to.equal(
+          1,
+          "dialPeer should be called for bootstrap peers"
+        );
+      });
+
+      it("should not be called more than DEFAULT_MAX_BOOTSTRAP_PEERS_ALLOWED times for bootstrap peers", async function () {
+        this.timeout(TEST_TIMEOUT);
+
+        // simulate that the peer is not connected
+        getConnectionsStub.returns([]);
+
+        // simulate that the peer is a bootstrap peer
+        getTagNamesForPeerStub.resolves([Tags.BOOTSTRAP]);
+
+        // emit first peer:discovery event
+        waku.libp2p.dispatchEvent(
+          new CustomEvent("peer:discovery", { detail: "bootstrap-peer" })
+        );
+
+        // simulate that the peer is connected
+        getConnectionsStub.returns([{ tags: [{ name: Tags.BOOTSTRAP }] }]);
+
+        // emit multiple peer:discovery events
+        const totalBootstrapPeers = 5;
+        for (let i = 1; i <= totalBootstrapPeers; i++) {
+          await delay(500);
+          waku.libp2p.dispatchEvent(
+            new CustomEvent("peer:discovery", {
+              detail: `bootstrap-peer-id-${i}`,
+            })
+          );
+        }
+
+        // check that dialPeer was called only once
+        expect(dialPeerStub.callCount).to.equal(
+          1,
+          "dialPeer should not be called more than once for bootstrap peers"
+        );
+      });
     });
 
-    it("should not be called more than DEFAULT_MAX_BOOTSTRAP_PEERS_ALLOWED times for bootstrap peers", async function () {
-      this.timeout(TEST_TIMEOUT);
+    describe("For peer-exchange peers", function () {
+      it("should be called for peers with PEER_EXCHANGE tags", async function () {
+        this.timeout(TEST_TIMEOUT);
 
-      // simulate that the peer is not connected
-      getConnectionsStub.returns([]);
+        // simulate that the peer is not connected
+        getConnectionsStub.returns([]);
 
-      // simulate that the peer is a bootstrap peer
-      getTagNamesForPeerStub.resolves([Tags.BOOTSTRAP]);
+        // simulate that the peer has a PEER_EXCHANGE tag
+        getTagNamesForPeerStub.resolves([Tags.PEER_EXCHANGE]);
 
-      // emit first peer:discovery event
-      waku.libp2p.dispatchEvent(
-        new CustomEvent("peer:discovery", { detail: "bootstrap-peer" })
-      );
-
-      // simulate that the peer is connected
-      getConnectionsStub.returns([{ tags: [{ name: Tags.BOOTSTRAP }] }]);
-
-      // emit multiple peer:discovery events
-      const totalBootstrapPeers = 5;
-      for (let i = 1; i <= totalBootstrapPeers; i++) {
-        await delay(500);
+        // emit a peer:discovery event
         waku.libp2p.dispatchEvent(
-          new CustomEvent("peer:discovery", {
-            detail: `bootstrap-peer-id-${i}`,
-          })
+          new CustomEvent("peer:discovery", { detail: "px-peer" })
         );
-      }
 
-      // check that dialPeer was called only once
-      expect(dialPeerStub.callCount).to.equal(
-        1,
-        "dialPeer should not be called more than once for bootstrap peers"
-      );
+        // wait for the async function calls within attemptDial to finish
+        await delay(DELAY_MS);
+
+        // check that dialPeer was called once
+        expect(dialPeerStub.callCount).to.equal(
+          1,
+          "dialPeer should be called for peers with PEER_EXCHANGE tags"
+        );
+      });
+
+      it("should be called for every peer with PEER_EXCHANGE tags", async function () {
+        this.timeout(TEST_TIMEOUT);
+
+        // simulate that the peer is not connected
+        getConnectionsStub.returns([]);
+
+        // simulate that the peer has a PEER_EXCHANGE tag
+        getTagNamesForPeerStub.resolves([Tags.PEER_EXCHANGE]);
+
+        // emit multiple peer:discovery events
+        const totalPxPeers = 5;
+        for (let i = 0; i < totalPxPeers; i++) {
+          waku.libp2p.dispatchEvent(
+            new CustomEvent("peer:discovery", { detail: `px-peer-id-${i}` })
+          );
+          await delay(500);
+        }
+
+        // check that dialPeer was called for each peer with PEER_EXCHANGE tags
+        expect(dialPeerStub.callCount).to.equal(totalPxPeers);
+      });
     });
   });
 });
