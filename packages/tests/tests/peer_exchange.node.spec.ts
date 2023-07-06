285e17909dfaae3fa8a5968affb5ebb33a1068a7
danisharora099 <danisharora099@gmail.com>
2023-07-06 01:31:20 +0100

    add tests to filter px peers based on shard: - px service nodes subscribed to the shard topic should be dialed - px service nodes not subscribed to the shard should not be dialed

--- packages/tests/tests/peer_exchange.node.spec.ts
+++ packages/tests/tests/peer_exchange.node.spec.ts
@@ -1,10 +1,11 @@
 import { bootstrap } from "@libp2p/bootstrap";
 import tests from "@libp2p/interface-peer-discovery-compliance-tests";
+import { PeerId } from "@libp2p/interface-peer-id";
 import {
   Fleet,
   getPredefinedBootstrapNodes,
 } from "@waku/core/lib/predefined_bootstrap_nodes";
-import type { LightNode, PeerInfo } from "@waku/interfaces";
+import { LightNode, PeerInfo, Tags } from "@waku/interfaces";
 import {
   PeerExchangeCodec,
   PeerExchangeDiscovery,
@@ -13,6 +14,7 @@ import {
 } from "@waku/peer-exchange";
 import { createLightNode, Libp2pComponents } from "@waku/sdk";
 import { expect } from "chai";
+import sinon, { SinonSpy } from "sinon";
 
 import { delay } from "../src/delay.js";
 import { makeLogFileName } from "../src/log_file.js";
@@ -146,7 +148,7 @@ describe("Peer Exchange", () => {
     });
   });
 
-  describe("compliance test", async function () {
+  describe("Compliance Test", async function () {
     this.timeout(55_000);
 
     let waku: LightNode;
@@ -205,6 +207,181 @@ describe("Peer Exchange", () => {
         !!nwaku2 && (await nwaku2.stop());
         !!waku && (await waku.stop());
       },
+    });
+  });
+
+  describe("Sharding", function () {
+    let waku: LightNode;
+    let nwaku1: NimGoNode;
+    let nwaku2: NimGoNode;
+    let nwaku3: NimGoNode;
+
+    let attemptDialSpy: SinonSpy;
+
+    beforeEach(async function () {
+      nwaku1 = new NimGoNode(makeLogFileName(this) + "1");
+      nwaku2 = new NimGoNode(makeLogFileName(this) + "2");
+      nwaku3 = new NimGoNode(makeLogFileName(this) + "3");
+    });
+
+    afterEach(async function () {
+      !!nwaku1 && nwaku1.stop();
+      !!nwaku2 && nwaku2.stop();
+      !!nwaku3 && nwaku3.stop();
+      !!waku && waku.stop().catch((e) => console.log("Waku failed to stop", e));
+
+      attemptDialSpy && attemptDialSpy.restore();
+    });
+
+    it.only("all px service nodes subscribed to the shard topic should be dialed", async function () {
+      this.timeout(100_000);
+
+      await nwaku1.start({
+        topic: "/waku/2/rs/18/2",
+        relay: true,
+        discv5Discovery: true,
+        peerExchange: true,
+      });
+
+      const enr1 = (await nwaku1.info()).enrUri;
+
+      await nwaku2.start({
+        topic: "/waku/2/rs/18/2",
+        relay: true,
+        discv5Discovery: true,
+        peerExchange: true,
+        discv5BootstrapNode: enr1,
+      });
+
+      const enr2 = (await nwaku2.info()).enrUri;
+
+      await nwaku3.start({
+        topic: "/waku/2/rs/18/2",
+        relay: true,
+        discv5Discovery: true,
+        peerExchange: true,
+        discv5BootstrapNode: enr2,
+      });
+      const nwaku3Ma = await nwaku3.getMultiaddrWithId();
+
+      waku = await createLightNode({
+        pubSubTopic: "/waku/2/rs/18/2",
+        libp2p: {
+          peerDiscovery: [
+            bootstrap({ list: [nwaku3Ma.toString()] }),
+            wakuPeerExchangeDiscovery(),
+          ],
+        },
+      });
+
+      await waku.start();
+
+      attemptDialSpy = sinon.spy(
+        (waku as any).connectionManager,
+        "attemptDial"
+      );
+
+      await new Promise<void>((resolve) => {
+        waku.libp2p.peerStore.addEventListener(
+          "change:protocols",
+          async (evt) => {
+            if (evt.detail.protocols.includes(PeerExchangeCodec)) {
+              resolve();
+            }
+          }
+        );
+      });
+
+      const pxPeersDiscovered = new Set<PeerId>();
+
+      await new Promise<void>((resolve) => {
+        waku.libp2p.addEventListener("peer:discovery", async (evt) => {
+          const peerId = evt.detail.id;
+          const tags = (await waku.libp2p.peerStore.getTags(peerId)).map(
+            (t) => t.name
+          );
+          if (tags.includes(Tags.PEER_EXCHANGE)) {
+            pxPeersDiscovered.add(peerId);
+            if (pxPeersDiscovered.size === 2) {
+              resolve();
+            }
+          }
+        });
+      });
+
+      await delay(1000);
+
+      expect(attemptDialSpy.callCount).to.equal(3);
+    });
+
+    it.only("px service nodes not subscribed to the shard should not be dialed", async function () {
+      this.timeout(100_000);
+
+      // this service node is not subscribed to the shard
+      await nwaku1.start({
+        topic: "/waku/2/rs/17/0",
+        relay: true,
+        discv5Discovery: true,
+        peerExchange: true,
+      });
+
+      const enr1 = (await nwaku1.info()).enrUri;
+
+      await nwaku2.start({
+        topic: "/waku/2/rs/18/2",
+        relay: true,
+        discv5Discovery: true,
+        peerExchange: true,
+        discv5BootstrapNode: enr1,
+      });
+
+      const enr2 = (await nwaku2.info()).enrUri;
+
+      await nwaku3.start({
+        relay: true,
+        discv5Discovery: true,
+        peerExchange: true,
+        discv5BootstrapNode: enr2,
+      });
+      const nwaku3Ma = await nwaku3.getMultiaddrWithId();
+
+      waku = await createLightNode({
+        pubSubTopic: "/waku/2/rs/18/2",
+        libp2p: {
+          peerDiscovery: [
+            bootstrap({ list: [nwaku3Ma.toString()] }),
+            wakuPeerExchangeDiscovery(),
+          ],
+        },
+      });
+
+      attemptDialSpy = sinon.spy(
+        (waku as any).connectionManager,
+        "attemptDial"
+      );
+
+      await waku.start();
+
+      const pxPeersDiscovered = new Set<PeerId>();
+
+      await new Promise<void>((resolve) => {
+        waku.libp2p.addEventListener("peer:discovery", async (evt) => {
+          const peerId = evt.detail.id;
+          const tags = (await waku.libp2p.peerStore.getTags(peerId)).map(
+            (t) => t.name
+          );
+          if (tags.includes(Tags.PEER_EXCHANGE)) {
+            pxPeersDiscovered.add(peerId);
+            if (pxPeersDiscovered.size === 1) {
+              resolve();
+            }
+          }
+        });
+      });
+
+      await delay(1000);
+
+      expect(attemptDialSpy.callCount).to.equal(2);
     });
   });
 });
