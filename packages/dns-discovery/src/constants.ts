5243e6262e47366bc0b7bd80393dcedd57cb57b9
danisharora099 <danisharora099@gmail.com>
2023-08-17 18:07:01 +0530

    fix linting errors

--- packages/dns-discovery/src/constants.ts
+++ packages/dns-discovery/src/constants.ts
@@ -2,7 +2,7 @@ import type { NodeCapabilityCount } from "@waku/interfaces";
 
 export const enrTree = {
   TEST: "enrtree://AOGECG2SPND25EEFMAJ5WF3KSGJNSGV356DSTL2YVLLZWIV6SAYBM@test.waku.nodes.status.im",
-  PROD: "enrtree://AOGECG2SPND25EEFMAJ5WF3KSGJNSGV356DSTL2YVLLZWIV6SAYBM@prod.waku.nodes.status.im",
+  PROD: "enrtree://AOGECG2SPND25EEFMAJ5WF3KSGJNSGV356DSTL2YVLLZWIV6SAYBM@prod.waku.nodes.status.im"
 };
 
 export const DEFAULT_BOOTSTRAP_TAG_NAME = "bootstrap";
@@ -12,5 +12,5 @@ export const DEFAULT_BOOTSTRAP_TAG_TTL = 100_000_000;
 export const DEFAULT_NODE_REQUIREMENTS: Partial<NodeCapabilityCount> = {
   store: 2,
   filter: 1,
-  lightPush: 1,
+  lightPush: 1
 };
