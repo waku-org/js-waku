dcb4fbff478ff703415627af0abbd424cbcd40ac
danisharora099 <danisharora099@gmail.com>
2023-09-05 11:14:46 +0530

    break down `getPeers` into a `filter` method

--- packages/core/src/lib/base_protocol.ts
+++ packages/core/src/lib/base_protocol.ts
@@ -69,6 +69,47 @@ export class BaseProtocol implements IBaseProtocol {
   /**
    * Retrieves a list of peers based on the specified criteria.
    *
+   * @param peers - The list of peers to filter from.
+   * @param numPeers - The total number of peers to retrieve. If 0, all peers are returned.
+   * @param maxBootstrapPeers - The maximum number of bootstrap peers to retrieve.
+   * @returns A Promise that resolves to an array of peers based on the specified criteria.
+   */
+  private async filterPeers(
+    peers: Peer[],
+    numPeers: number,
+    maxBootstrapPeers: number
+  ): Promise<Peer[]> {
+    // Collect the bootstrap peers up to the specified maximum
+    const bootstrapPeers = peers
+      .filter((peer) => peer.tags.has(Tags.BOOTSTRAP))
+      .slice(0, maxBootstrapPeers);
+
+    // Collect non-bootstrap peers
+    const nonBootstrapPeers = peers.filter(
+      (peer) => !peer.tags.has(Tags.BOOTSTRAP)
+    );
+
+    // If numPeers is 0, return all peers
+    if (numPeers === 0) {
+      return [...bootstrapPeers, ...nonBootstrapPeers];
+    }
+
+    // Initialize the list of selected peers with the bootstrap peers
+    const selectedPeers: Peer[] = [...bootstrapPeers];
+
+    // Fill up to numPeers with remaining random peers if needed
+    while (selectedPeers.length < numPeers && nonBootstrapPeers.length > 0) {
+      const randomIndex = Math.floor(Math.random() * nonBootstrapPeers.length);
+      const randomPeer = nonBootstrapPeers.splice(randomIndex, 1)[0];
+      selectedPeers.push(randomPeer);
+    }
+
+    return selectedPeers;
+  }
+
+  /**
+   * Retrieves a list of peers based on the specified criteria.
+   *
    * @param numPeers - The total number of peers to retrieve. If 0, all peers are returned.
    * @param maxBootstrapPeers - The maximum number of bootstrap peers to retrieve.
    * @returns A Promise that resolves to an array of peers based on the specified criteria.
@@ -85,32 +126,8 @@ export class BaseProtocol implements IBaseProtocol {
       this.multicodec
     ]);
 
-    // Collect the bootstrap peers up to the specified maximum
-    const bootstrapPeers = allPeersForProtocol
-      .filter((peer) => peer.tags.has(Tags.BOOTSTRAP))
-      .slice(0, maxBootstrapPeers);
-
-    // Collect non-bootstrap peers
-    const remainingPeers = allPeersForProtocol.filter(
-      (peer) => !bootstrapPeers.includes(peer)
-    );
-
-    // If numPeers is 0, return all peers
-    if (numPeers === 0) {
-      return [...bootstrapPeers, ...remainingPeers];
-    }
-
-    // Initialize the list of selected peers with the bootstrap peers
-    const selectedPeers: Peer[] = [...bootstrapPeers];
-
-    // Fill up to numPeers with remaining random peers if needed
-    while (selectedPeers.length < numPeers && remainingPeers.length > 0) {
-      const randomIndex = Math.floor(Math.random() * remainingPeers.length);
-      const randomPeer = remainingPeers.splice(randomIndex, 1)[0];
-      selectedPeers.push(randomPeer);
-    }
-
-    return selectedPeers;
+    // Filter the peers based on the specified criteria
+    return this.filterPeers(allPeersForProtocol, numPeers, maxBootstrapPeers);
   }
 
   protected async newStream(peer: Peer): Promise<Stream> {
