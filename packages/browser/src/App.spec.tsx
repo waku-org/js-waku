870979575f6637368813c70287f63fc99b6b3aae
Sasha <oleksandr@status.im>
2023-09-07 01:26:39 +0200

    add playwright and experiment with karma

---
+++ packages/browser/src/App.spec.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { test, expect } from '@playwright/experimental-ct-react';
+import App from './App';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('should work', async ({ mount }) => {
+  const component = await mount(<App />);
+  await expect(component).toContainText('Learn React');
+});
