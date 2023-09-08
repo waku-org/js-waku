fe611c1954a121d687233885a6007b2d27daf5e5
Sasha <oleksandr@status.im>
2023-09-08 19:36:09 +0200

    remove tsconfig

--- packages/browser-tests/tests/web-chat.spec.ts
+++ packages/browser-tests/tests/web-chat.spec.ts
@@ -2,7 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("has title Web Chat title", async ({ page }) => {
   await page.goto("");
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("Waku v2 chat app");
 });
