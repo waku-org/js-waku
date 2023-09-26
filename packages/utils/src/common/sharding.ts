a626ceb81cdc6c55a36294dfc19161d5e9134096
Danish Arora <35004822+danisharora099@users.noreply.github.com>
2023-09-26 20:23:35 +0530

    update error message
    Co-authored-by: fryorcraken <110212804+fryorcraken@users.noreply.github.com>

--- packages/utils/src/common/sharding.ts
+++ packages/utils/src/common/sharding.ts
@@ -6,7 +6,7 @@ export function ensurePubsubTopicIsValid(
 ): void {
   if (!configuredTopics.includes(pubsubTopic)) {
     throw new Error(
-      `PubSub topic ${pubsubTopic} is not supported by this protocol. Configured topics are: ${configuredTopics}. Please update your configuration by passing in the topic during Waku node instantiation.`
+      `PubSub topic ${pubsubTopic} has not been configured on this instance. Configured topics are: ${configuredTopics}. Please update your configuration by passing in the topic during Waku node instantiation.`
     );
   }
 }
