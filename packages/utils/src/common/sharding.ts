d20a565fe170d96707e50b42b3ece5e2f6ecb2e6
danisharora099 <danisharora099@gmail.com>
2023-09-29 15:23:00 +0530

    utils: add shard<-> bytes conversion helpers

--- packages/utils/src/common/sharding.ts
+++ packages/utils/src/common/sharding.ts
@@ -1,5 +1,7 @@
 import type { PubSubTopic, ShardInfo } from "@waku/interfaces";
 
+import { bytesToUtf8, utf8ToBytes } from "../bytes";
+
 export const getPubsubTopicsFromShardInfo = (
   shardInfo: ShardInfo
 ): PubSubTopic[] => {
@@ -17,4 +19,13 @@ export function ensurePubsubTopicIsConfigured(
       `PubSub topic ${pubsubTopic} has not been configured on this instance. Configured topics are: ${configuredTopics}. Please update your configuration by passing in the topic during Waku node instantiation.`
     );
   }
+}
+
+//TODO: this can be generalized
+export function shardInfoToBytes(shardInfo: ShardInfo): Uint8Array {
+  return utf8ToBytes(JSON.stringify(shardInfo));
+}
+
+export function bytesToShardInfo(bytes: Uint8Array): ShardInfo {
+  return JSON.parse(bytesToUtf8(bytes));
 }
