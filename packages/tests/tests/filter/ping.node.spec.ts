fe4d644464e05b62a680c16fbc6d80b4111c333b
danisharora099 <danisharora099@gmail.com>
2023-10-12 13:21:04 +0530

    add a test that uses ping to check filter subscription

--- packages/tests/tests/filter/ping.node.spec.ts
+++ packages/tests/tests/filter/ping.node.spec.ts
@@ -3,7 +3,12 @@ import type { IFilterSubscription, LightNode } from "@waku/interfaces";
 import { utf8ToBytes } from "@waku/utils/bytes";
 import { expect } from "chai";
 
-import { MessageCollector, NimGoNode, tearDownNodes } from "../../src/index.js";
+import {
+  // delay,
+  MessageCollector,
+  NimGoNode,
+  tearDownNodes
+} from "../../src/index.js";
 
 import {
   runNodes,
@@ -58,5 +63,41 @@ describe("Waku Filter V2: Ping", function () {
 
     // Ping imediately after unsubscribe
     await validatePingError(subscription);
+  });
+
+  it("Reopen subscription with peer with lost subscription", async function () {
+    const openSubscription = async (): Promise<void> => {
+      await subscription.subscribe([TestDecoder], messageCollector.callback);
+    };
+
+    const unsubscribe = async (): Promise<void> => {
+      await subscription.unsubscribe([TestContentTopic]);
+    };
+
+    const pingAndReinitiateSubscription = async (): Promise<void> => {
+      try {
+        await subscription.ping();
+      } catch (error) {
+        if (
+          error instanceof Error &&
+          error.message.includes("peer has no subscriptions")
+        ) {
+          await openSubscription();
+        } else {
+          throw error;
+        }
+      }
+    };
+
+    // open subscription & ping -> should pass
+    await openSubscription();
+    await pingAndReinitiateSubscription();
+
+    // unsubscribe & ping -> should fail and reinitiate subscription
+    await unsubscribe();
+    await pingAndReinitiateSubscription();
+
+    // ping -> should pass as subscription is reinitiated
+    await pingAndReinitiateSubscription();
   });
 });
