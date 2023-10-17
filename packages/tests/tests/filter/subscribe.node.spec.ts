50e7cff331eda885828062ee8963f574004692ca
fbarbu15 <barbu_florin_adrian@yahoo.com>
2023-10-17 09:11:12 +0300

    fail a test for demo

--- packages/tests/tests/filter/subscribe.node.spec.ts
+++ packages/tests/tests/filter/subscribe.node.spec.ts
@@ -62,7 +62,10 @@ describe("Waku Filter V2: Subscribe", function () {
       expectedMessageText: messageText,
       expectedContentTopic: TestContentTopic
     });
-    expect((await nwaku.messages()).length).to.eq(1);
+    expect(
+      (await nwaku.messages()).length,
+      "Intentional failure for allure report"
+    ).to.eq(2);
   });
 
   it("Subscribe and receive messages via waku relay post", async function () {
