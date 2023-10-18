9fa1e544d1b97ed8cf3b66d23ee76776f9685331
danisharora099 <danisharora099@gmail.com>
2023-10-18 17:36:21 +0530

    setup a custom Logger with log level support

--- packages/utils/.eslintrc.cjs
+++ packages/utils/.eslintrc.cjs
@@ -2,5 +2,13 @@ module.exports = {
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: "./tsconfig.dev.json"
-  }
+  },
+  overrides: [
+    {
+      files: ["src/logger/index.ts"],
+      rules: {
+        "no-console": "off"
+      }
+    }
+  ]
 };
