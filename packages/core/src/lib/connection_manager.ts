1892f5093da540530d7ee5640178ebaa46cf769f
Danish Arora <35004822+danisharora099@users.noreply.github.com>
2023-10-20 18:16:48 +0530

    fix: don't dial discovered peers if have already been attempted dial (#1657)
    * don't dial peers if they exist in PeerStore already
    
    * fix(tests): bugs & add types to dispatch event
    
    * fix: more tests
    
    * fix: dial validation
    
    * update doc & reduce delay
    
    * refactor test
    
    * use -1 instead of Infinity
    
    * fix comment
    
    * fix rebase

--- packages/core/src/lib/connection_manager.ts
+++ packages/core/src/lib/connection_manager.ts
@@ -38,7 +38,7 @@ export class ConnectionManager
   private dialAttemptsForPeer: Map<string, number> = new Map();
   private dialErrorsForPeer: Map<string, any> = new Map();
 
-  private currentActiveDialCount = 0;
+  private currentActiveParallelDialCount = 0;
   private pendingPeerDialQueue: Array<PeerId> = [];
 
   public static create(
@@ -183,7 +183,7 @@ export class ConnectionManager
   }
 
   private async dialPeer(peerId: PeerId): Promise<void> {
-    this.currentActiveDialCount += 1;
+    this.currentActiveParallelDialCount += 1;
     let dialAttempt = 0;
     while (dialAttempt < this.options.maxDialAttemptsForPeer) {
       try {
@@ -199,7 +199,10 @@ export class ConnectionManager
           conn.tags = Array.from(new Set([...conn.tags, ...tags]));
         });
 
-        this.dialAttemptsForPeer.delete(peerId.toString());
+        // instead of deleting the peer from the peer store, we set the dial attempt to -1
+        // this helps us keep track of peers that have been dialed before
+        this.dialAttemptsForPeer.set(peerId.toString(), -1);
+
         // Dialing succeeded, break the loop
         break;
       } catch (error) {
@@ -224,7 +227,7 @@ export class ConnectionManager
     }
 
     // Always decrease the active dial count and process the dial queue
-    this.currentActiveDialCount--;
+    this.currentActiveParallelDialCount--;
     this.processDialQueue();
 
     // If max dial attempts reached and dialing failed, delete the peer
@@ -276,7 +279,7 @@ export class ConnectionManager
   private processDialQueue(): void {
     if (
       this.pendingPeerDialQueue.length > 0 &&
-      this.currentActiveDialCount < this.options.maxParallelDials
+      this.currentActiveParallelDialCount < this.options.maxParallelDials
     ) {
       const peerId = this.pendingPeerDialQueue.shift();
       if (!peerId) return;
@@ -322,7 +325,7 @@ export class ConnectionManager
   private async attemptDial(peerId: PeerId): Promise<void> {
     if (!(await this.shouldDialPeer(peerId))) return;
 
-    if (this.currentActiveDialCount >= this.options.maxParallelDials) {
+    if (this.currentActiveParallelDialCount >= this.options.maxParallelDials) {
       this.pendingPeerDialQueue.push(peerId);
       return;
     }
@@ -404,6 +407,7 @@ export class ConnectionManager
    * 1. If the peer is already connected, don't dial
    * 2. If the peer is not part of any of the configured pubsub topics, don't dial
    * 3. If the peer is not dialable based on bootstrap status, don't dial
+   * 4. If the peer is already has an active dial attempt, or has been dialed before, don't dial it
    * @returns true if the peer should be dialed, false otherwise
    */
   private async shouldDialPeer(peerId: PeerId): Promise<boolean> {
@@ -433,6 +437,14 @@ export class ConnectionManager
     if (!(await this.isPeerDialableBasedOnBootstrapStatus(peerId))) {
       log.warn(
         `Peer ${peerId.toString()} is not dialable based on bootstrap status. Not dialing.`
+      );
+      return false;
+    }
+
+    // If the peer is already already has an active dial attempt, or has been dialed before, don't dial it
+    if (this.dialAttemptsForPeer.has(peerId.toString())) {
+      log.warn(
+        `Peer ${peerId.toString()} has already been attempted dial before, or already has a dial attempt in progress, skipping dial`
       );
       return false;
     }
