686a6983dbe49de6549d07b8bbbbc354bd81e4d0
Sasha <oleksandr@status.im>
2023-11-27 23:46:04 +0100

    add test

---
+++ packages/tests/tests/create.optional.spec.ts
@@ -0,0 +1,31 @@
+import { createLightNode, LightNode } from "@waku/sdk";
+import { expect } from "chai";
+import sinon, { SinonSpy } from "sinon";
+
+import { tearDownNodes } from "../src/index.js";
+
+describe.only("Craete node", () => {
+  let waku: LightNode;
+  let consoleInfoSpy: SinonSpy;
+
+  beforeEach(() => {
+    consoleInfoSpy = sinon.spy(console as any, "info");
+  });
+
+  afterEach(async () => {
+    consoleInfoSpy.restore();
+    await tearDownNodes([], waku);
+  });
+
+  it("should log info about WebSocket failures to console when hideWebSocketInfo disabled", async () => {
+    waku = await createLightNode();
+    expect(consoleInfoSpy.callCount).to.be.equal(2);
+  });
+
+  it("should not log info about WebSocket failures to console when hideWebSocketInfo enabled", async () => {
+    waku = await createLightNode({
+      libp2p: { hideWebSocketInfo: true }
+    });
+    expect(consoleInfoSpy.callCount).to.be.equal(0);
+  });
+});
