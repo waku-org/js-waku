4bdd7eeafdcede6e4dc91268146e4adce169bd68
danisharora099 <danisharora099@gmail.com>
2024-01-10 13:00:15 +0530

    add a comment discouraging relay

--- packages/sdk/src/create.ts
+++ packages/sdk/src/create.ts
@@ -80,6 +80,12 @@ export async function createLightNode(
 /**
  * Create a Waku node that uses Waku Relay to send and receive messages,
  * enabling some privacy preserving properties.
+ * * @remarks
+ * This function creates a Relay Node using the Waku Relay protocol.
+ * While it is technically possible to use this function in a browser environment,
+ * it is not recommended due to potential performance issues and limited browser capabilities.
+ * If you are developing a browser-based application, consider alternative approaches like creating a Light Node
+ * or use this function with caution.
  */
 export async function createRelayNode(
   options?: ProtocolCreateOptions & WakuOptions & Partial<RelayCreateOptions>
