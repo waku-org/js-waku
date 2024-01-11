dc96074c810c145ea7eaafc215b498206b3b11f3
Danish Arora <35004822+danisharora099@users.noreply.github.com>
2024-01-11 16:39:48 +0530

    fix(tests): peer-exchange interop (#1773)
    * fix(tests): px interop
    
    * optimize waitFor utility
    
    * address minor comments
    
    * fix: test refactoring

--- packages/tests/tests/peer_exchange.node.spec.ts
+++ packages/tests/tests/peer_exchange.node.spec.ts
@@ -1,4 +1,6 @@
+import type { PeerId } from "@libp2p/interface/peer-id";
 import tests from "@libp2p/interface-compliance-tests/peer-discovery";
+import type { Multiaddr } from "@multiformats/multiaddr";
 import type { LightNode, PeerInfo } from "@waku/interfaces";
 import {
   PeerExchangeCodec,
@@ -9,7 +11,7 @@ import { createLightNode, Libp2pComponents } from "@waku/sdk";
 import { expect } from "chai";
 
 import { delay } from "../src/delay.js";
-import { tearDownNodes } from "../src/index.js";
+import { tearDownNodes, waitForRemotePeerWithCodec } from "../src/index.js";
 import { makeLogFileName } from "../src/log_file.js";
 import { NimGoNode } from "../src/node/node.js";
 
@@ -48,11 +50,13 @@ describe("Peer Exchange", () => {
       });
 
       const nwaku1PeerId = await nwaku1.getPeerId();
+      const nwaku2PeerId = await nwaku2.getPeerId();
       const nwaku2Ma = await nwaku2.getMultiaddrWithId();
 
       waku = await createLightNode();
       await waku.start();
       await waku.libp2p.dialProtocol(nwaku2Ma, PeerExchangeCodec);
+      await waitForRemotePeerWithCodec(waku, PeerExchangeCodec, nwaku2PeerId);
 
       const components = waku.libp2p.components as unknown as Libp2pComponents;
       const peerExchange = new WakuPeerExchange(components);
@@ -62,6 +66,7 @@ describe("Peer Exchange", () => {
       let peerInfos: PeerInfo[] = [];
       while (peerInfos.length <= 0) {
         peerInfos = (await peerExchange.query({
+          peerId: nwaku2PeerId,
           numPeers: numPeersToRequest
         })) as PeerInfo[];
         await delay(3000);
@@ -70,16 +75,35 @@ describe("Peer Exchange", () => {
       expect(peerInfos.length).to.be.greaterThan(0);
       expect(peerInfos.length).to.be.lessThanOrEqual(numPeersToRequest);
       expect(peerInfos[0].ENR).to.not.be.null;
+      expect(peerInfos[0].ENR?.peerInfo?.multiaddrs).to.not.be.null;
 
-      const doesPeerIdExistInResponse =
-        peerInfos.find(
-          ({ ENR }) => ENR?.peerInfo?.id.toString() === nwaku1PeerId.toString()
-        ) !== undefined;
+      let foundNodeMultiaddrs: Multiaddr[] = [];
+      let foundNodePeerId: PeerId | undefined = undefined;
+      const doesPeerIdExistInResponse = peerInfos.some(({ ENR }) => {
+        foundNodeMultiaddrs = ENR?.peerInfo?.multiaddrs ?? [];
+        foundNodePeerId = ENR?.peerInfo?.id;
+        return ENR?.peerInfo?.id.toString() === nwaku1PeerId.toString();
+      });
+
+      if (!foundNodePeerId) {
+        throw new Error("Peer ID not found");
+      }
+
+      if (!foundNodePeerId) {
+        throw new Error("Peer ID not found");
+      }
 
       expect(doesPeerIdExistInResponse).to.be.equal(true);
 
-      expect(await waku.libp2p.peerStore.has(await nwaku2.getPeerId())).to.be
-        .true;
+      await waku.libp2p.dialProtocol(foundNodeMultiaddrs, PeerExchangeCodec);
+      await waitForRemotePeerWithCodec(
+        waku,
+        PeerExchangeCodec,
+        foundNodePeerId
+      );
+
+      expect(await waku.libp2p.peerStore.has(nwaku1PeerId)).to.eq(true);
+      expect(waku.libp2p.getConnections()).has.length(2);
     });
   });
 
