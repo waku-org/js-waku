dd5768ee9acf53d7e7b154ea89320d1cc6a3f414
danisharora099 <danisharora099@gmail.com>
2024-02-16 19:47:41 +0530

    rm: only

--- packages/local-discovery/src/local_discovery.spec.ts
+++ packages/local-discovery/src/local_discovery.spec.ts
@@ -39,7 +39,7 @@ async function setPeersInLocalStorage(
   localStorage.setItem("waku:peers", JSON.stringify(peers));
 }
 
-describe.only("Local Storage Discovery", function () {
+describe("Local Storage Discovery", function () {
   this.timeout(25_000);
   let components: Libp2pComponents;
 
