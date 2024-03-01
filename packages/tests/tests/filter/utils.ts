427bfc43f6d92352aca417f79c1caf693725b1fc
Sasha <oleksandr@status.im>
2024-03-01 01:36:49 +0100

    fix test as REST does not return peer though connection is made

--- packages/tests/tests/filter/utils.ts
+++ packages/tests/tests/filter/utils.ts
@@ -106,7 +106,7 @@ export async function runMultipleNodes(
     const wakuConnections = waku.libp2p.getConnections();
     const nodePeers = await node.peers();
 
-    if (wakuConnections.length < 1 || nodePeers.length < 1) {
+    if (wakuConnections.length < 1 && nodePeers.length < 1) {
       throw new Error(
         `Expected at least 1 peer in each node. Got waku connections: ${wakuConnections.length} and service nodes: ${nodePeers.length}`
       );
