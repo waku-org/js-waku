f181f32d7f7e97107f0a9bb15336ff17b6d1a7ef
danisharora099 <danisharora099@gmail.com>
2024-03-11 19:49:26 +0530

    chore: metadata protocol uses error codes

--- packages/interfaces/src/metadata.ts
+++ packages/interfaces/src/metadata.ts
@@ -1,11 +1,25 @@
 import type { PeerId } from "@libp2p/interface";
 
 import type { ShardInfo } from "./enr.js";
-import type { IBaseProtocolCore, ShardingParams } from "./protocols.js";
+import type {
+  IBaseProtocolCore,
+  ProtocolError,
+  ShardingParams
+} from "./protocols.js";
+
+export type QueryResult =
+  | {
+      shardInfo: ShardInfo;
+      error: null;
+    }
+  | {
+      shardInfo: null;
+      error: ProtocolError;
+    };
 
 // IMetadata always has shardInfo defined while it is optionally undefined in IBaseProtocol
 export interface IMetadata extends Omit<IBaseProtocolCore, "shardInfo"> {
   shardInfo: ShardingParams;
-  confirmOrAttemptHandshake(peerId: PeerId): Promise<void>;
-  query(peerId: PeerId): Promise<ShardInfo | undefined>;
+  confirmOrAttemptHandshake(peerId: PeerId): Promise<QueryResult>;
+  query(peerId: PeerId): Promise<QueryResult>;
 }
