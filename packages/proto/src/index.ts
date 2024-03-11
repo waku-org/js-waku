82da484197f3bebb50d35958a363a508cee4adac
danisharora099 <danisharora099@gmail.com>
2024-03-11 15:44:41 +0530

    chore: have src/generated as a dedicated directory for generated proto code

--- packages/proto/src/index.ts
+++ packages/proto/src/index.ts
@@ -1,17 +1,17 @@
-export * as proto_message from "./lib/message.js";
-export { WakuMessage } from "./lib/filter.js";
+export * as proto_message from "./generated/message.js";
+export { WakuMessage } from "./generated/filter.js";
 
-export * as proto_topic_only_message from "./lib/topic_only_message.js";
-export { TopicOnlyMessage } from "./lib/topic_only_message.js";
+export * as proto_topic_only_message from "./generated/topic_only_message.js";
+export { TopicOnlyMessage } from "./generated/topic_only_message.js";
 
-export * as proto_filter from "./lib/filter.js";
-export * as proto_filter_v2 from "./lib/filter_v2.js";
+export * as proto_filter from "./generated/filter.js";
+export * as proto_filter_v2 from "./generated/filter_v2.js";
 
-export * as proto_lightpush from "./lib/light_push.js";
-export { PushResponse } from "./lib/light_push.js";
+export * as proto_lightpush from "./generated/light_push.js";
+export { PushResponse } from "./generated/light_push.js";
 
-export * as proto_store from "./lib/store.js";
+export * as proto_store from "./generated/store.js";
 
-export * as proto_peer_exchange from "./lib/peer_exchange.js";
+export * as proto_peer_exchange from "./generated/peer_exchange.js";
 
-export * as proto_metadata from './lib/metadata.js'
+export * as proto_metadata from './generated/metadata.js'
