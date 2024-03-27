23fc08d7d72d4c44e1b66b16bc73c518e445be12
Sasha <oleksandr@status.im>
2024-03-27 13:29:59 +0100

    add peer dependencies and other improvements

--- packages/polyfills/src/index.ts
+++ packages/polyfills/src/index.ts
@@ -1,3 +1,2 @@
 import "event-target-polyfill";
 import "text-encoding-polyfill";
-import "react-native-url-polyfill/auto";
--- packages/polyfills/src/metro.ts
+++ packages/polyfills/src/metro.ts
@@ -1,6 +1,8 @@
 import type { MetroConfig } from "expo/metro-config";
 
-export function setupWakuMetroConfig(config: MetroConfig): void {
+type Writeable<T> = { -readonly [P in keyof T]: Writeable<T[P]> };
+
+export function setupWakuMetroConfig(config: Writeable<MetroConfig>): void {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const path = require("path");
 
