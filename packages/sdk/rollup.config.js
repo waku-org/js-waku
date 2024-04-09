1bc47b702a887c831e2a953e9f7c18efed812835
Sasha <oleksandr@status.im>
2024-04-09 13:10:34 +0300

    up package name

--- packages/sdk/rollup.config.js
+++ packages/sdk/rollup.config.js
@@ -1,8 +1,8 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import { extractExports } from "@waku/build-utils";
-import replace from "rollup-plugin-replace";
 
 import * as packageJson from "./package.json" assert { type: "json" };
 
