be8b314d844cfc80c0f8c299f48e56805d2e6138
danisharora099 <danisharora099@gmail.com>
2024-06-04 10:23:23 -0400

    chore: move store v2 to a subdir

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -12,10 +12,10 @@ export { FilterCore, FilterCodecs } from "./lib/filter/index.js";
 export * as waku_light_push from "./lib/light_push/index.js";
 export { LightPushCodec, LightPushCore } from "./lib/light_push/index.js";
 
-export * as waku_store from "./lib/store/index.js";
-export { StoreCore } from "./lib/store/index.js";
+export * as waku_store from "./lib/store/v2/index.js";
+export { StoreCore } from "./lib/store/v2/index.js";
 
-export { PageDirection } from "./lib/store/index.js";
+export { PageDirection } from "./lib/store/v2/index.js";
 
 export { waitForRemotePeer } from "./lib/wait_for_remote_peer.js";
 
