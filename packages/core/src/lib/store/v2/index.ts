7daf1bbaf53bf822a3dc331f7b0b44eab92e7e64
danisharora099 <danisharora099@gmail.com>
2024-06-04 10:23:23 -0400

    chore: move store v2 to a subdir

--- packages/core/src/lib/store/v2/index.ts
+++ packages/core/src/lib/store/v2/index.ts
@@ -14,8 +14,8 @@ import * as lp from "it-length-prefixed";
 import { pipe } from "it-pipe";
 import { Uint8ArrayList } from "uint8arraylist";
 
-import { BaseProtocol } from "../base_protocol.js";
-import { toProtoMessage } from "../to_proto_message.js";
+import { BaseProtocol } from "../../base_protocol.js";
+import { toProtoMessage } from "../../to_proto_message.js";
 
 import { HistoryRpc, PageDirection, Params } from "./history_rpc.js";
 
