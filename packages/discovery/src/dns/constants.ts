6ab3c5b2cdbb9aa76d68f0879114b479c4721137
danisharora099 <danisharora099@gmail.com>
2024-06-04 13:50:10 -0400

    fix: bootstrapping into default pubsub topic

--- packages/discovery/src/dns/constants.ts
+++ packages/discovery/src/dns/constants.ts
@@ -1,8 +1,9 @@
 import type { NodeCapabilityCount } from "@waku/interfaces";
 
 export const enrTree = {
-  TEST: "enrtree://AOGYWMBYOUIMOENHXCHILPKY3ZRFEULMFI4DOM442QSZ73TT2A7VI@test.waku.nodes.status.im",
-  SANDBOX:
+  DEFAULT_PUBSUB:
+    "enrtree://ANEDLO25QVUGJOUTQFRYKWX6P4Z4GKVESBMHML7DZ6YK4LGS5FC5O@prod.wakuv2.nodes.status.im",
+  TWN_SANDBOX:
     "enrtree://AIRVQ5DDA4FFWLRBCHJWUWOO6X6S4ZTZ5B667LQ6AJU6PEYDLRD5O@sandbox.waku.nodes.status.im"
 };
 
--- packages/sdk/src/utils/discovery.ts
+++ packages/sdk/src/utils/discovery.ts
@@ -5,7 +5,11 @@ import {
   wakuLocalPeerCacheDiscovery,
   wakuPeerExchangeDiscovery
 } from "@waku/discovery";
-import { type Libp2pComponents, PubsubTopic } from "@waku/interfaces";
+import {
+  DefaultPubsubTopic,
+  type Libp2pComponents,
+  PubsubTopic
+} from "@waku/interfaces";
 
 const DEFAULT_NODE_REQUIREMENTS = {
   lightPush: 1,
@@ -16,10 +20,20 @@ const DEFAULT_NODE_REQUIREMENTS = {
 export function defaultPeerDiscoveries(
   pubsubTopics: PubsubTopic[]
 ): ((components: Libp2pComponents) => PeerDiscovery)[] {
-  const discoveries = [
-    wakuDnsDiscovery([enrTree["SANDBOX"]], DEFAULT_NODE_REQUIREMENTS),
+  const discoveries = [];
+  discoveries.push(
     wakuLocalPeerCacheDiscovery(),
     wakuPeerExchangeDiscovery(pubsubTopics)
-  ];
+  );
+
+  //TODO: Add support for The Waku Network
+  //TODO: add issue here
+  const isDefaultPubsub = pubsubTopics.includes(DefaultPubsubTopic);
+  if (isDefaultPubsub) {
+    discoveries.push(
+      wakuDnsDiscovery([enrTree["DEFAULT_PUBSUB"]], DEFAULT_NODE_REQUIREMENTS)
+    );
+  }
+
   return discoveries;
 }
