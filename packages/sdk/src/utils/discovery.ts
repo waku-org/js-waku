0adbf6da435fa3d7b2ea9d3fb1dfac0f95ca2194
danisharora099 <danisharora099@gmail.com>
2024-06-05 15:44:34 -0400

    feat: use TWN fleets, or wakuv2.prod if DefaultPubsubTopic

--- packages/sdk/src/utils/discovery.ts
+++ packages/sdk/src/utils/discovery.ts
@@ -20,29 +20,16 @@ const DEFAULT_NODE_REQUIREMENTS = {
 export function defaultPeerDiscoveries(
   pubsubTopics: PubsubTopic[]
 ): ((components: Libp2pComponents) => PeerDiscovery)[] {
-  const discoveries = [];
-  discoveries.push(
+  const isDefaultPubsubTopic = pubsubTopics.includes(DefaultPubsubTopic);
+  const dnsEnrTrees = isDefaultPubsubTopic
+    ? [enrTree["DEPRECATED_DEFAULT_PUBSUB"]]
+    : [enrTree["SANDBOX"], enrTree["TEST"]];
+
+  const discoveries = [
+    wakuDnsDiscovery(dnsEnrTrees, DEFAULT_NODE_REQUIREMENTS),
     wakuLocalPeerCacheDiscovery(),
     wakuPeerExchangeDiscovery(pubsubTopics)
-  );
-
-  //TODO: Add support for The Waku Network
-  //https://github.com/waku-org/nwaku/issues/2593
-  const isDefaultPubsub = pubsubTopics.includes(DefaultPubsubTopic);
-  if (isDefaultPubsub) {
-    discoveries.push(
-      wakuDnsDiscovery([enrTree["DEPRECATED_DEFAULT_PUBSUB"]], DEFAULT_NODE_REQUIREMENTS)
-    );
-  }
+  ];
 
   return discoveries;
-}
-
-
-export function xdefaultPeerDiscoveries(
-  pubsubTopics: PubsubTopic[]
-): ((components: Libp2pComponents) => PeerDiscovery)[] {
-  const isDefaultPubsubTopic = pubsubTopics.includes(DefaultPubsubTopic);
-
-  const dnsEnrTree = isDefaultPubsubTopic ? enrTree["DEPRECATED_DEFAULT_PUBSUB"] : undefined;
 }
