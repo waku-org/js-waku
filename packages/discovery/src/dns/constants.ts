e53966964a30be3e1208cea463b87b7685703c79
Sasha <oleksandr@status.im>
2024-09-01 17:24:07 +0200

    fix: improve node bootstrapping

--- packages/discovery/src/dns/constants.ts
+++ packages/discovery/src/dns/constants.ts
@@ -15,7 +15,7 @@ export const DEFAULT_BOOTSTRAP_TAG_VALUE = 50;
 export const DEFAULT_BOOTSTRAP_TAG_TTL = 100_000_000;
 
 export const DEFAULT_NODE_REQUIREMENTS: Partial<NodeCapabilityCount> = {
-  store: 2,
-  filter: 1,
-  lightPush: 1
+  store: 1,
+  filter: 2,
+  lightPush: 2
 };
--- packages/sdk/src/create/discovery.ts
+++ packages/sdk/src/create/discovery.ts
@@ -7,19 +7,13 @@ import {
 } from "@waku/discovery";
 import { type Libp2pComponents, PubsubTopic } from "@waku/interfaces";
 
-const DEFAULT_NODE_REQUIREMENTS = {
-  lightPush: 1,
-  filter: 1,
-  store: 1
-};
-
 export function defaultPeerDiscoveries(
   pubsubTopics: PubsubTopic[]
 ): ((components: Libp2pComponents) => PeerDiscovery)[] {
   const dnsEnrTrees = [enrTree["SANDBOX"], enrTree["TEST"]];
 
   const discoveries = [
-    wakuDnsDiscovery(dnsEnrTrees, DEFAULT_NODE_REQUIREMENTS),
+    wakuDnsDiscovery(dnsEnrTrees),
     wakuLocalPeerCacheDiscovery(),
     wakuPeerExchangeDiscovery(pubsubTopics)
   ];
