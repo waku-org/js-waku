e77fbec31e5cacffcf49b4f5beae8cd9d945a9ce
Sasha <oleksandr@status.im>
2024-10-01 11:43:15 +0200

    chore: add debug log

--- packages/tests/tests/filter/single_node/subscribe.node.spec.ts
+++ packages/tests/tests/filter/single_node/subscribe.node.spec.ts
@@ -280,9 +280,11 @@ describe("Waku Filter V2: Subscribe: Single Service Node", function () {
 
     // Send a unique message on each topic.
     for (let i = 0; i < topicCount; i++) {
+      const now = Date.now();
       await waku.lightPush.send(td.encoders[i], {
         payload: utf8ToBytes(`Message for Topic ${i + 1}`)
       });
+      console.log("DEBUG:", Date.now() - now);
     }
 
     // Verify that each message was received on the corresponding topic.
