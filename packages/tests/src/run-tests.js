ca9bd98058e4a8302ff67eb63e15bced85728605
Danish Arora <danisharora099@gmail.com>
2025-01-15 16:08:51 +0530

    chore: upgrade to nwaku 0.34.0

--- packages/tests/src/run-tests.js
+++ packages/tests/src/run-tests.js
@@ -3,7 +3,7 @@ import { promisify } from "util";
 
 const execAsync = promisify(exec);
 
-const WAKUNODE_IMAGE = process.env.WAKUNODE_IMAGE || "wakuorg/nwaku:v0.33.1";
+const WAKUNODE_IMAGE = process.env.WAKUNODE_IMAGE || "wakuorg/nwaku:v0.34.0";
 
 async function main() {
   try {
