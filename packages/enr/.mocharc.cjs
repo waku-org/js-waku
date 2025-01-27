c99ad8d1e2167b672eab9c8e7e8e1642250afa08
Danish Arora <danisharora099@gmail.com>
2025-01-27 20:17:02 +0530

    chore: switch out of allure

--- packages/enr/.mocharc.cjs
+++ packages/enr/.mocharc.cjs
@@ -1,23 +1,23 @@
 const config = {
   extension: ['ts'],
-  spec: 'src/**/*.spec.ts',
   require: ['ts-node/register', 'isomorphic-fetch'],
   loader: 'ts-node/esm',
   'node-option': [
     'experimental-specifier-resolution=node',
     'loader=ts-node/esm'
   ],
-  exit: true
+  exit: true,
+  retries: 4
 };
 
 if (process.env.CI) {
   console.log("Running tests in parallel");
   config.parallel = true;
   config.jobs = 6;
-  console.log("Activating allure reporting");
-  config.reporter = 'mocha-multi-reporters';
+  console.log("Using JSON reporter for test results");
+  config.reporter = 'json';
   config.reporterOptions = {
-    configFile: '.mocha.reporters.json'
+    output: 'reports/mocha-results.json'
   };
 } else {
   console.log("Running tests serially. To enable parallel execution update mocha config");
