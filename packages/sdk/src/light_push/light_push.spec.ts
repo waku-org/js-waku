4280e0881f9bbfac8b4d66e7fa2266014f36d756
Sasha <oleksandr@status.im>
2025-02-04 21:53:29 +0100

    up tests

--- packages/sdk/src/light_push/light_push.spec.ts
+++ packages/sdk/src/light_push/light_push.spec.ts
@@ -1,4 +1,4 @@
-import { Peer } from "@libp2p/interface";
+import { Peer, PeerId } from "@libp2p/interface";
 import {
   ConnectionManager,
   createEncoder,
@@ -17,7 +17,7 @@ import { LightPush } from "./light_push.js";
 const PUBSUB_TOPIC = "/waku/2/rs/1/4";
 const CONTENT_TOPIC = "/test/1/waku-light-push/utf8";
 
-describe("LightPush SDK", () => {
+describe.only("LightPush SDK", () => {
   let libp2p: Libp2p;
   let encoder: Encoder;
   let lightPush: LightPush;
@@ -59,8 +59,8 @@ describe("LightPush SDK", () => {
 
     lightPush = mockLightPush({ libp2p, numPeersToUse: 2 });
     let sendSpy = sinon.spy(
-      (_encoder: any, _message: any, peer: Peer) =>
-        ({ success: peer.id }) as any
+      (_encoder: any, _message: any, peerId: PeerId) =>
+        ({ success: peerId }) as any
     );
     lightPush.protocol.send = sendSpy;
 
@@ -74,8 +74,8 @@ describe("LightPush SDK", () => {
     // check if setting another value works
     lightPush = mockLightPush({ libp2p, numPeersToUse: 3 });
     sendSpy = sinon.spy(
-      (_encoder: any, _message: any, peer: Peer) =>
-        ({ success: peer.id }) as any
+      (_encoder: any, _message: any, peerId: PeerId) =>
+        ({ success: peerId }) as any
     );
     lightPush.protocol.send = sendSpy;
 
@@ -91,9 +91,9 @@ describe("LightPush SDK", () => {
     });
 
     lightPush = mockLightPush({ libp2p });
-    let sendSpy = sinon.spy((_encoder: any, _message: any, peer: Peer) => {
-      if (peer.id.toString() === "1") {
-        return { success: peer.id };
+    let sendSpy = sinon.spy((_encoder: any, _message: any, peerId: PeerId) => {
+      if (peerId.toString() === "1") {
+        return { success: peerId };
       }
 
       return { failure: { error: "problem" } };
