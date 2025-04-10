3c44d2e76ba14c15c8d3312ce2018af5eb441db4
Sasha <oleksandr@status.im>
2025-04-11 00:30:10 +0200

    remove comment

--- packages/tests/src/lib/dockerode.ts
+++ packages/tests/src/lib/dockerode.ts
@@ -161,9 +161,6 @@ export default class Dockerode {
         }
         if (stream) {
           stream.pipe(logStream);
-          // stream.on("data", (chunk) => {
-          // console.log(chunk.toString());
-          // });
         }
       }
     );
