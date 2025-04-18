00b54bda8d6f5fa94ccddcf2f0dcb1ecac940935
Danish Arora <danisharora099@gmail.com>
2025-04-18 14:59:31 +0530

    feat: enable/disable discoveries without defaultBootstrap: true

---
+++ packages/sdk/src/create/discovery.spec.ts
@@ -0,0 +1,49 @@
+import { PubsubTopic } from "@waku/interfaces";
+import { expect } from "chai";
+
+import { defaultPeerDiscoveries } from "./discovery.js";
+
+describe.only("Default Peer Discoveries", () => {
+  const pubsubTopics: PubsubTopic[] = [];
+
+  it("should enable all discoveries by default", () => {
+    const discoveries = defaultPeerDiscoveries(pubsubTopics);
+    expect(discoveries.length).to.equal(3);
+  });
+
+  it("should disable DNS discovery when specified", () => {
+    const discoveries = defaultPeerDiscoveries(pubsubTopics, {
+      dns: false,
+      peerExchange: true,
+      localPeerCache: true
+    });
+    expect(discoveries.length).to.equal(2);
+  });
+
+  it("should disable Peer Exchange discovery when specified", () => {
+    const discoveries = defaultPeerDiscoveries(pubsubTopics, {
+      dns: true,
+      peerExchange: false,
+      localPeerCache: true
+    });
+    expect(discoveries.length).to.equal(2);
+  });
+
+  it("should disable Local Peer Cache discovery when specified", () => {
+    const discoveries = defaultPeerDiscoveries(pubsubTopics, {
+      dns: true,
+      peerExchange: true,
+      localPeerCache: false
+    });
+    expect(discoveries.length).to.equal(2);
+  });
+
+  it("should disable multiple discoveries when specified", () => {
+    const discoveries = defaultPeerDiscoveries(pubsubTopics, {
+      dns: false,
+      peerExchange: false,
+      localPeerCache: true
+    });
+    expect(discoveries.length).to.equal(1);
+  });
+});
