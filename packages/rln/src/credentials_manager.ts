32ac5277dca89e789b1fbeba239e33e5801ca754
Danish Arora <danisharora099@gmail.com>
2025-06-11 15:25:55 +0530

    use non deprecated sha256

--- packages/rln/src/credentials_manager.ts
+++ packages/rln/src/credentials_manager.ts
@@ -1,5 +1,5 @@
 import { hmac } from "@noble/hashes/hmac";
-import { sha256 } from "@noble/hashes/sha256";
+import { sha256 } from "@noble/hashes/sha2";
 import { Logger } from "@waku/utils";
 import { ethers } from "ethers";
 
