6a21fe1fa077120d3b1902841ddfc441504f3aa3
Sasha <oleksandr@status.im>
2025-07-01 16:01:54 +0200

    unskip Filter e2e test

--- packages/tests/tests/filter/subscribe.node.spec.ts
+++ packages/tests/tests/filter/subscribe.node.spec.ts
@@ -293,8 +293,7 @@ const runTests = (strictCheckNodes: boolean): void => {
       });
     });
 
-    // skip for now, will be enabled once old Filter is removed as it exausts amount of streams avaialble
-    it.skip("Subscribe to 30 topics in separate streams (30 streams for Filter is limit) at once and receives messages", async function () {
+    it("Subscribe to 30 topics in separate streams (30 streams for Filter is limit) at once and receives messages", async function () {
       this.timeout(100_000);
       const topicCount = 30;
       const td = generateTestData(topicCount, { pubsubTopic: TestPubsubTopic });
