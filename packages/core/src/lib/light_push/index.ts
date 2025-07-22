5553b04cf346ca33e7d3e6a0c94f86d03cb6aaa6
Arseniy Klempner <arseniyk@status.im>
2025-07-22 14:59:38 -0700

    feat: handle both light push protocols

--- packages/core/src/lib/light_push/index.ts
+++ packages/core/src/lib/light_push/index.ts
@@ -5,3 +5,29 @@ export {
   LightPushCodecs,
   PushResponse
 } from "./light_push.js";
+
+export { PushRpcV3, LightPushV3StatusCodes } from "./push_rpc_v3.js";
+
+// Alias exports to minimize explicit V3 naming while keeping backward compatibility
+export { PushRpcV3 as PushRpcLatest } from "./push_rpc_v3.js";
+import { LightPushCodecV3 } from "./light_push.js";
+/**
+ * Latest Light Push multicodec (currently v3). Alias for LightPushCodecV3.
+ * @deprecated Prefer `LightPushCodecLatest`. `LightPushCodecV3` will be kept for backward compatibility.
+ */
+export const LightPushCodecLatest = LightPushCodecV3;
+import { LightPushV3StatusCodes } from "./push_rpc_v3.js";
+/**
+ * Alias for LightPushV3StatusCodes to avoid explicit version suffix in most code paths.
+ */
+export const LightPushStatusCodes = LightPushV3StatusCodes;
+
+export {
+  isRLNResponseError,
+  isSuccess,
+  toLightPushError,
+  toProtocolError,
+  getStatusDescription,
+  getProtocolVersion,
+  inferProtocolVersion
+} from "./utils.js";
