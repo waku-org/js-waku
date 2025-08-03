202d728ab316bdf62421b31a3a7b51b234e64323
fryorcraken <commits@fryorcraken.xyz>
2025-08-03 22:54:31 +1000

    SDS: add bloom filter test
    # Conflicts:
    #	packages/sds/src/message_channel/events.spec.ts

--- packages/sds/src/message_channel/events.spec.ts
+++ packages/sds/src/message_channel/events.spec.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 
 import { DefaultBloomFilter } from "../bloom_filter/bloom.js";
 
-import { decodeMessage, encodeMessage, Message } from "./events.js";
+import { Message } from "./events.js";
 import { DEFAULT_BLOOM_FILTER_OPTIONS } from "./message_channel.js";
 
 describe("Message serialization", () => {
@@ -12,16 +12,17 @@ describe("Message serialization", () => {
     const bloomFilter = new DefaultBloomFilter(DEFAULT_BLOOM_FILTER_OPTIONS);
     bloomFilter.insert(messageId);
 
-    const message: Message = {
-      messageId: "123",
-      channelId: "my-channel",
-      causalHistory: [],
-      lamportTimestamp: 0,
-      bloomFilter: bloomFilter.toBytes()
-    };
+    const message = new Message(
+      "123",
+      "my-channel",
+      [],
+      0,
+      bloomFilter.toBytes(),
+      undefined
+    );
 
-    const bytes = encodeMessage(message);
-    const decMessage = decodeMessage(bytes);
+    const bytes = message.encode();
+    const decMessage = Message.decode(bytes);
 
     const decBloomFilter = DefaultBloomFilter.fromBytes(
       decMessage!.bloomFilter!,
