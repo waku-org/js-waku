200b07ff3e0a0a88dba1104cd722fd40342187e2
fryorcraken <commits@fryorcraken.xyz>
2025-08-28 16:04:59 +1000

    remove `channel` prefix

--- packages/sdk/src/reliable_channel/events.ts
+++ packages/sdk/src/reliable_channel/events.ts
@@ -7,7 +7,7 @@ export enum ReliableChannelEvent {
    *
    * This event may be emitted several times if the retry mechanism kicks in.
    */
-  OutMessageSending = "channel:out:message-sending",
+  OutMessageSending = "out:message-sending",
   /**
    * The message has been sent over the wire but has not been acknowledged by
    * any other party yet.
@@ -17,7 +17,7 @@ export enum ReliableChannelEvent {
    * This event may be emitted several times if the
    * several times if the retry mechanisms kicks in.
    */
-  OutMessageSent = "channel:out:message-sent",
+  OutMessageSent = "out:message-sent",
   /**
    * A received bloom filter seems to indicate that the messages was received
    * by another party.
@@ -25,29 +25,29 @@ export enum ReliableChannelEvent {
    * However, this is probabilistic. The retry mechanism will wait a bit longer
    * before trying to send the message again.
    */
-  OutMessagePossiblyAcknowledged = "channel:out:message-possibly-acknowledged",
+  OutMessagePossiblyAcknowledged = "out:message-possibly-acknowledged",
   /**
    * The message was fully acknowledged by other members of the channel
    */
-  OutMessageAcknowledged = "channel:out:message-acknowledged",
+  OutMessageAcknowledged = "out:message-acknowledged",
   /**
    * The retry mechanism failed too many times, and the message is not considered
    * as sent. TODO: implement
    */
-  OutMessageRetriesError = "channel:out:message-retries-error",
+  OutMessageRetriesError = "out:message-retries-error",
   /**
    * It was not possible to send the messages due to a non-recoverable error,
    * most likely an internal error for a developer to resolve.
    */
-  OutMessageIrrecoverableError = "channel:out:message-irrecoverable-error",
+  OutMessageIrrecoverableError = "out:message-irrecoverable-error",
   /**
    * A new message has been received.
    */
-  InMessageReceived = "channel:in:message-received",
+  InMessageReceived = "in:message-received",
   /**
    * We are aware of a missing message but failed to retrieve it successfully.
    */
-  InIrretrievableMessage = "channel:in:message-irretrievable"
+  InIrretrievableMessage = "in:message-irretrievable"
 }
 
 export type ReliableChannelEvents = {
